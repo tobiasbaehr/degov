67517830234ec663f4b57237c23da9d9f1de7050
Peter Majmesku <majmesku@publicplan.de>
2018-02-16 16:06:18 +0100

    DEGOV-170: CI für deGov ermöglichen

---
+++ ci/pipelines-test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+php ./phpunit-4.8.36.phar
---
+++ tests/CiTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class CiTest extends TestCase
+{
+	public function testTest(): void
+	{
+		$this->assertEquals(true, true);
+	}
+
+}
