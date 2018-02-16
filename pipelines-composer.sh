72b5f84e565df288ac535290ca414dbc04da14d8
Peter Majmesku <majmesku@publicplan.de>
2018-02-16 17:37:38 +0100

    DEGOV-170: CI für deGov ermöglichen

--- pipelines-composer.sh
+++ pipelines-composer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-php /usr/local/bin/composer create-project degov/degov-project --dev
-php /usr/local/bin/composer require phpunit/phpunit:~4.8 --dev
+php /usr/local/bin/composer create-project degov/degov-project
+php /usr/local/bin/composer require phpunit/phpunit:~4.8
