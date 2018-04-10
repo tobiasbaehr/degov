03b74c7a24580904fe8c35f56f8c320906b69e91
Peter Majmesku <majmesku@publicplan.de>
2018-04-10 16:49:35 +0200

    Switched tabs to spaces.

--- modules/degov_media_gallery/js/gallery.js
+++ modules/degov_media_gallery/js/gallery.js
@@ -41,26 +41,26 @@
         Drupal.behaviors.gallery.pswpItems.push($pswpItem);
       });
 
-        $('.slick-controls__gallery', $gallery).once().append('<span class="slick__download"><a href="' + settings.degov_media_gallery.imagesDownloadLinks[0].uri + '"><i aria-hidden="true" class="fa fa-download"></i>' + Drupal.t('Download') + '</a></span>');
+      $('.slick-controls__gallery', $gallery).once().append('<span class="slick__download"><a href="' + settings.degov_media_gallery.imagesDownloadLinks[0].uri + '"><i aria-hidden="true" class="fa fa-download"></i>' + Drupal.t('Download') + '</a></span>');
 
-        if (settings.degov_media_gallery.imagesDownloadLinks[0].field_allow_download === "0") {
-            $('.slick-controls__gallery .slick__download').hide();
-            $('.slick-controls__gallery .slick__lightroom').css("right", "0px");
-        } else if(settings.degov_media_gallery.imagesDownloadLinks[0].field_allow_download === "1") {
-            $('.slick-controls__gallery .slick__download').show();
-            $('.slick-controls__gallery .slick__lightroom').css("right", "129px");
-        }
+      if (settings.degov_media_gallery.imagesDownloadLinks[0].field_allow_download === "0") {
+        $('.slick-controls__gallery .slick__download').hide();
+        $('.slick-controls__gallery .slick__lightroom').css("right", "0px");
+      } else if (settings.degov_media_gallery.imagesDownloadLinks[0].field_allow_download === "1") {
+        $('.slick-controls__gallery .slick__download').show();
+        $('.slick-controls__gallery .slick__lightroom').css("right", "129px");
+      }
 
-        $slider.find('.media-image').click(function () {
-          var $index = parseInt($slider.slick('slickCurrentSlide'));
+      $slider.find('.media-image').click(function () {
+        var $index = parseInt($slider.slick('slickCurrentSlide'));
 
-          var $options = {
-            index: $index
-          };
-          // Initializes and opens PhotoSwipe.
-          var $pswp = new PhotoSwipe($pswpElement, PhotoSwipeUI_Default, Drupal.behaviors.gallery.pswpItems, $options);
-          $pswp.init();
-        });
+        var $options = {
+          index: $index
+        };
+        // Initializes and opens PhotoSwipe.
+        var $pswp = new PhotoSwipe($pswpElement, PhotoSwipeUI_Default, Drupal.behaviors.gallery.pswpItems, $options);
+        $pswp.init();
+      });
       $('.media-gallery-js-open-lightroom', $gallery).click(function () {
         $images.get($slider.slick('slickCurrentSlide')).click();
       });
@@ -68,11 +68,11 @@
         var i = (currentSlide ? currentSlide : 0) + 1;
 
         if (settings.degov_media_gallery.imagesDownloadLinks[$slider.slick('slickCurrentSlide')].field_allow_download === "0") {
-            $('.slick-controls__gallery .slick__download').hide();
-            $('.slick-controls__gallery .slick__lightroom').css("right", "0px");
-        } else if(settings.degov_media_gallery.imagesDownloadLinks[$slider.slick('slickCurrentSlide')].field_allow_download === "1") {
-            $('.slick-controls__gallery .slick__download').show();
-            $('.slick-controls__gallery .slick__lightroom').css("right", "129px");
+          $('.slick-controls__gallery .slick__download').hide();
+          $('.slick-controls__gallery .slick__lightroom').css("right", "0px");
+        } else if (settings.degov_media_gallery.imagesDownloadLinks[$slider.slick('slickCurrentSlide')].field_allow_download === "1") {
+          $('.slick-controls__gallery .slick__download').show();
+          $('.slick-controls__gallery .slick__lightroom').css("right", "129px");
         }
 
         $('.slick__counter__current', $gallery).text(i);
