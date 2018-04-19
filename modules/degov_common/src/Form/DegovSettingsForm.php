c8d20154229e92b58a365345aa4ed866815def24
Tim Bureck <me@tbureck.de>
2018-04-19 11:05:59 +0200

    * Made privacy URL and netiquette URL for social media settings modal configurable * Default values are previously hard coded values

--- modules/degov_common/src/Form/DegovSettingsForm.php
+++ modules/degov_common/src/Form/DegovSettingsForm.php
@@ -45,13 +45,27 @@ class DegovSettingsForm extends ConfigFormBase {
       '#type' => 'markup',
       '#markup' => '<p>' . $this->t('This is configuration section of deGov distribution') . '</p>',
     ];
+    $form['privacy_url'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Privacy URL Path'),
+        '#description' => $this->t('The path at which the privacy terms can be found.'),
+        '#default_value' => $config->get('privacy_url') ?: '/datenschutzhinweise',
+        '#required' => true
+    ];
+    $form['netiquette_url'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Netiquette URL Path'),
+        '#description' => $this->t('The path at which the netiquette can be found.'),
+        '#default_value' => $config->get('netiquette_url') ?: '/netiquette',
+        '#required' => true
+    ];
     $form['youtube_apikey'] = array(
       '#type' => 'textfield',
       '#title' => t('Youtube API Key'),
       '#description' => $this->t('Obtain a Youtube API key at <a href="@link">@link</a>', array(
         '@link' => 'https://console.developers.google.com/apis/credentials',
       )),
-      '#default_value' => $config->get('youtube_apikey', ''),
+      '#default_value' => $config->get('youtube_apikey') ?: '',
       '#required' => FALSE,
     );
     return parent::buildForm($form, $form_state);
@@ -62,7 +76,10 @@ class DegovSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = \Drupal::service('config.factory')->getEditable('degov_common.default_settings');
-    $config->set('youtube_apikey', $form_state->getValue('youtube_apikey'))
+    $config
+        ->set('privacy_url', $form_state->getValue('privacy_url'))
+        ->set('netiquette_url', $form_state->getValue('netiquette_url'))
+        ->set('youtube_apikey', $form_state->getValue('youtube_apikey'))
       ->save();
 
     parent::submitForm($form, $form_state);
