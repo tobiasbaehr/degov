266255267212fa644e34e567356e61286113d173
Peter Majmesku <majmesku@publicplan.de>
2018-05-03 23:19:02 +0200

    Implemented way to disable some feed by js cookie check. Twitter block is weired.

--- js/instagram_slider.js
+++ js/instagram_slider.js
@@ -10,32 +10,39 @@
   // Slick slider in Twitter block
   Drupal.behaviors.slickInstagram = {
     attach: function (context, settings) {
-      $('.instagram-preview').once().slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        autoplay: true,
-        responsive: [
-          {
-            breakpoint: 1319,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
 
-      $('.slick__pause').on('click', function () {
-        $('.instagram-preview').once().slick('slickPause');
-        $(this).hide().siblings('.slick__play').show();
-      });
-      $('.slick__play').on('click', function () {
-        $('.instagram-preview').once().slick('slickPlay');
-        $(this).hide().siblings('.slick__pause').show();
-      });
+      if ($.parseJSON($.cookie("degov_social_media_settings")).instagram) {
+        $('.instagram-preview').once().slick({
+          dots: true,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 1319,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+
+        $('.slick__pause').on('click', function () {
+          $('.instagram-preview').once().slick('slickPause');
+          $(this).hide().siblings('.slick__play').show();
+        });
+        $('.slick__play').on('click', function () {
+          $('.instagram-preview').once().slick('slickPlay');
+          $(this).hide().siblings('.slick__pause').show();
+        });
+      } else {
+        var dataSafetyInfo = Drupal.t('This social media source is disabled. You can enable it in the <a href="#" class="js-social-media-settings-open">social media settings</a>.');
+        $('.instagram-preview').html(dataSafetyInfo);
+      }
+
     }
   };
 
