46079eafdbcfe7943f493d6516b426d1728deab5
Peter Majmesku <majmesku@publicplan.de>
2018-05-03 16:44:03 +0200

    On ajax requesting.

--- js/youtube_slider.js
+++ js/youtube_slider.js
@@ -10,31 +10,38 @@
   // Slick slider in Youtube block
   Drupal.behaviors.slickYoutube = {
     attach: function (context, settings) {
-      $('.youtube-preview').once().slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        autoplay: true,
-        responsive: [
-          {
-            breakpoint: 1319,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+      $.get( "/youtube-feed-ajax", function( data ) {
+        $( ".youtube-feed-block" ).html( data ).promise().done(function(){
+          $('.youtube-preview').once().slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            autoplay: true,
+            responsive: [
+              {
+                breakpoint: 1319,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
 
-      $('.slick__pause').on('click', function () {
-        $('.youtube-preview').once().slick('slickPause');
-        $(this).hide().siblings('.slick__play').show();
-      });
-      $('.slick__play').on('click', function () {
-        $('.youtube-preview').once().slick('slickPlay');
-        $(this).hide().siblings('.slick__pause').show();
+          $('.slick__pause').on('click', function () {
+            $('.youtube-preview').once().slick('slickPause');
+            $(this).hide().siblings('.slick__play').show();
+          });
+          $('.slick__play').on('click', function () {
+            $('.youtube-preview').once().slick('slickPlay');
+            $(this).hide().siblings('.slick__pause').show();
+          });
+        });
+
+        console.log('foo');
+
       });
     }
   };
--- src/Plugin/Block/YoutubeFeedBlock.php
+++ src/Plugin/Block/YoutubeFeedBlock.php
@@ -7,6 +7,7 @@ use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\ContextAwarePluginInterface;
 use Drupal\degov_social_media_settings\Service\CookieCheck;
+use Drupal\degov_theming\Service\Template;
 use Drupal\facets\Exception\Exception;
 use Drupal\youtube_feed_block\Factory\YouTubeFactory;
 use Madcoda\Youtube\Youtube;
@@ -73,54 +74,10 @@ class YoutubeFeedBlock extends BlockBase implements ContextAwarePluginInterface,
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-
-    $config = \Drupal::service('config.factory')
-      ->get('youtube_feed_block.settings');
-
-    $apiKey = $config->get('api_key');
-    $channelName = $config->get('channel');
-    $numberOfVideos = $config->get('number_of_videos');
-
-    try {
-      $youtube = $this->youtubeFactory->create($apiKey);;
-
-      $channelID = $youtube->getChannelByName($channelName)->id;
-      if(!$channelID) $channelID = $channelName;
-      $videos = $youtube->searchChannelVideos('', $channelID, $numberOfVideos, 'date');
-
-
-      foreach ($videos as $video) {
-        $info = $youtube->getVideoInfo($video->id->videoId);
-
-        $build['youtube_feed_block'][] = [
-          '#theme' => 'youtube_feed_block',
-          '#title' => $video->snippet->title,
-          '#likes' => $info->statistics->likeCount,
-          '#views' => $info->statistics->viewCount,
-          '#comments' => $info->statistics->commentCount,
-          '#videoID' => $video->id->videoId,
-          '#thumbnail' => $video->snippet->thumbnails->default->url,
-          '#cache' => ['max-age' => (60 * 60)],
-          '#link_display' => $this->_shortDescription("https://youtube.com/watch?v=" . $video->id->videoId, 32, '...'),
-        ];
-      }
-    }
-    catch(Exception $e) {
-      $build['#markup'] = '<span class="error">'.$e."</span>";
-
-    }
+    $build['#markup'] = '<div class="youtube-feed-block"></div>';
 
     return $build;
   }
 
-  function _shortDescription($string, $maxLenth, $replacement) {
-    if (strlen($string) > $maxLenth) {
-      return substr($string, 0, $maxLenth) . $replacement;
-    }
-    else {
-      return $string;
-    }
-  }
 }
 
