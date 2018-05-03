f84b15e104c07bef91e9fde00364d1e121c0f87c
Peter Majmesku <majmesku@publicplan.de>
2018-05-03 23:19:02 +0200

    Implemented way to disable some feed by js cookie check. Twitter block is weired.

--- modules/degov_tweets/js/tweets_slider.js
+++ modules/degov_tweets/js/tweets_slider.js
@@ -3,39 +3,50 @@
  *
  * Defines the behavior of the Slideshow paragraph.
  */
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal) {
 
   'use strict';
 
   // Slick slider in Twitter block
   Drupal.behaviors.slickTweets = {
     attach: function (context, settings) {
-      $('.tweets-slideshow .tweets').once().slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        autoplay: true,
-        responsive: [
-          {
-            breakpoint: 720,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
 
-      $('.slick__pause').on('click', function () {
-        $('.tweets__slides').once().slick('slickPause');
-        $(this).hide().siblings('.slick__play').show();
-      });
-      $('.slick__play').on('click', function () {
-        $('.tweets__slides').once().slick('slickPlay');
-        $(this).hide().siblings('.slick__pause').show();
-      });
+      console.log('Muhh!!');
+
+      console.log($.parseJSON($.cookie("degov_social_media_settings")).twitter);
+
+      if ($.parseJSON($.cookie("degov_social_media_settings")).twitter) {
+        $('.tweets-slideshow .tweets').once().slick({
+          dots: true,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 1319,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+
+        $('.slick__pause').on('click', function () {
+          $('.youtube-preview').once().slick('slickPause');
+          $(this).hide().siblings('.slick__play').show();
+        });
+        $('.slick__play').on('click', function () {
+          $('.youtube-preview').once().slick('slickPlay');
+          $(this).hide().siblings('.slick__pause').show();
+        });
+      } else {
+        var dataSafetyInfo = Drupal.t('This social media source is disabled. You can enable it in the <a href="#" class="js-social-media-settings-open">social media settings</a>.');
+        $('.tweets-slideshow .tweets').html(dataSafetyInfo);
+      }
+
     }
   };
 
