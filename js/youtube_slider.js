8a1c6d0ea92ad424d65c6eb57dcc6e388b2b4c4f
Peter Majmesku <majmesku@publicplan.de>
2018-05-03 23:19:02 +0200

    Implemented way to disable some feed by js cookie check. Twitter block is weired.

--- js/youtube_slider.js
+++ js/youtube_slider.js
@@ -10,39 +10,38 @@
   // Slick slider in Youtube block
   Drupal.behaviors.slickYoutube = {
     attach: function (context, settings) {
-      $.get( "/youtube-feed-ajax", function( data ) {
-        $( ".youtube-feed-block" ).html( data ).promise().done(function(){
-          $('.youtube-preview').once().slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            autoplay: true,
-            responsive: [
-              {
-                breakpoint: 1319,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
+      if ($.parseJSON($.cookie("degov_social_media_settings")).youtube) {
+        $('.youtube-preview').once().slick({
+          dots: true,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 1319,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
               }
-            ]
-          });
-
-          $('.slick__pause').on('click', function () {
-            $('.youtube-preview').once().slick('slickPause');
-            $(this).hide().siblings('.slick__play').show();
-          });
-          $('.slick__play').on('click', function () {
-            $('.youtube-preview').once().slick('slickPlay');
-            $(this).hide().siblings('.slick__pause').show();
-          });
+            }
+          ]
         });
 
-        console.log('foo');
+        $('.slick__pause').on('click', function () {
+          $('.youtube-preview').once().slick('slickPause');
+          $(this).hide().siblings('.slick__play').show();
+        });
+        $('.slick__play').on('click', function () {
+          $('.youtube-preview').once().slick('slickPlay');
+          $(this).hide().siblings('.slick__pause').show();
+        });
+      } else {
+        var dataSafetyInfo = Drupal.t('This social media source is disabled. You can enable it in the <a href="#" class="js-social-media-settings-open">social media settings</a>.');
+        $('.youtube-preview').html(dataSafetyInfo);
+      }
 
-      });
     }
   };
 
--- src/Plugin/Block/YoutubeFeedBlock.php
+++ src/Plugin/Block/YoutubeFeedBlock.php
@@ -3,81 +3,60 @@
 namespace Drupal\youtube_feed_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Config\ConfigFactory;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Plugin\ContextAwarePluginInterface;
-use Drupal\degov_social_media_settings\Service\CookieCheck;
-use Drupal\degov_theming\Service\Template;
-use Drupal\facets\Exception\Exception;
-use Drupal\youtube_feed_block\Factory\YouTubeFactory;
 use Madcoda\Youtube\Youtube;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Template\TwigEnvironment;
 
 /**
- * Provides a 'YouTubeFeedBlock' block.
+ * Provides a 'YouttubeFeedBlock' block.
  *
  * @Block(
  *  id = "youtube_feed_block",
  *  admin_label = @Translation("Youtube Feed Block"),
  * )
  */
-class YoutubeFeedBlock extends BlockBase implements ContextAwarePluginInterface, ContainerFactoryPluginInterface {
-
-  /**
-   * @var ConfigFactory
-   */
-  private $configFactory;
-
-  /**
-   * @var TwigEnvironment
-   */
-  private $twig;
-
-  /**
-   * @var CookieCheck
-   */
-  private $cookieCheck;
-
-  /**
-   * @var YouTubeFactory
-   */
-  private $youtubeFactory;
-
-  public function __construct(
-    array $configuration, $plugin_id, $plugin_definition, ConfigFactory $configFactory, YouTubeFactory $youTubeFactory, CookieCheck $cookieCheck, TwigEnvironment $twig
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->configFactory = $configFactory;
-    $this->youtubeFactory = $youTubeFactory;
-    $this->cookieCheck = $cookieCheck;
-    $this->twig = $twig;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('config.factory'),
-      $container->get('youtube_feed_block.youtube_factory'),
-      $container->get('degov_social_media_settings.cookie_check'),
-      $container->get('twig')
-    );
-  }
+class YoutubeFeedBlock extends BlockBase {
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build['#markup'] = '<div class="youtube-feed-block"></div>';
+    $build = [];
+
+    $config = \Drupal::service('config.factory')
+      ->get('youtube_feed_block.settings');
+
+    $apiKey = $config->get('api_key');
+    $channelName = $config->get('channel');
+    $numberOfVideos = $config->get('number_of_videos');
+
+    $youtube = new Youtube(['key' => $apiKey]);
+    $channelID = $youtube->getChannelByName($channelName)->id;
+    $videos = $youtube->searchChannelVideos('', $channelID, $numberOfVideos, 'date');
+
+    foreach ($videos as $video) {
+      $info = $youtube->getVideoInfo($video->id->videoId);
+      $build['youtube_feed_block'][] = [
+        '#theme' => 'youtube_feed_block',
+        '#title' => $video->snippet->title,
+        '#likes' => $info->statistics->likeCount,
+        '#views' => $info->statistics->viewCount,
+        '#comments' => $info->statistics->commentCount,
+        '#videoID' => $video->id->videoId,
+        '#thumbnail' =>$video->snippet->thumbnails->default->url,
+        '#cache' => ['max-age' => (60*60)],
+        '#link_display' => $this->_shortDescription("https://youtube.com/watch?q=".$video->id->videoId,32,'...'),
+      ];
+    }
 
     return $build;
   }
 
+  function _shortDescription($string, $maxLenth, $replacement) {
+    if (strlen($string) > $maxLenth) {
+      return substr($string, 0, $maxLenth) . $replacement;
+    }
+    else {
+      return $string;
+    }
+  }
 }
 
