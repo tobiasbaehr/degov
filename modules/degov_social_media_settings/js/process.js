ff53f54dd934d6b647b930b73902115716abf35a
Sascha Hannes <hannes@publicplan.de>
2018-06-25 15:27:24 +0200

    Added YT Slider

--- modules/degov_social_media_settings/js/process.js
+++ modules/degov_social_media_settings/js/process.js
@@ -89,8 +89,18 @@
         initTwitter(wrapper);
       }
 
+      if (source === 'twitter') {
+        initTwitter(wrapper);
+        initSoMeSlider('twitter');
+      }
+
       if (source === 'instagram') {
         initInstagram();
+        initSoMeSlider('instagram');
+      }
+
+      if (source === 'youtube') {
+        initSoMeSlider('youtube');
       }
     }
     else {
@@ -198,6 +208,39 @@
 
   // Initialize.
   initializeSettings();
+
+  function initSoMeSlider(source) {
+    var selector = source === 'twitter' ? $('.tweets-slideshow .tweets') : $('.' + source + '-preview');
+    if (selector.hasClass("slick-slider")) {
+      selector.slick('unslick');
+    }
+    selector.slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 1319,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    $('.slick__pause').on('click', function () {
+      selector.once().slick('slickPause');
+      $(this).hide().siblings('.slick__play').show();
+    });
+    $('.slick__play').on('click', function () {
+      selector.once().slick('slickPlay');
+      $(this).hide().siblings('.slick__pause').show();
+    });
+  }
 
 })(jQuery, Drupal, drupalSettings);
 
