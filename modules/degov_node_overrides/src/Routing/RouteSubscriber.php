4c75e691565b46ddaa67de0b001d5a83d8af7d16
Peter Majmesku <majmesku@publicplan.de>
2018-07-09 16:57:31 +0200

    #2984568: All replacements of nrwgov

---
+++ modules/degov_node_overrides/src/Routing/RouteSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\degov_node_overrides\Routing;
+
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * Alters existing routes for a specific collection.
+   *
+   * @param \Symfony\Component\Routing\RouteCollection $collection
+   *   The route collection for adding routes.
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('entity.node.canonical')) {
+      $route->setDefault('_controller', '\Drupal\degov_node_overrides\Controller\NodeViewController::view');
+    }
+  }
+}
