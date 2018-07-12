77fa50f4e48521d110f64a8c3ae7edabc56ec505
Sascha Hannes <hannes@publicplan.de>
2018-07-12 19:53:55 +0200

    tag command correct

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 echo "    StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 git clone git@bitbucket.org:/publicplan/degov_project.git
 cd degov_project
@@ -7,6 +6,6 @@ composer update degov/degov
 git add composer.lock
 git commit -m "Updating deGov dependencies automatically"
 git push
-TAG=./docroot/profiles/degov/scripts/transform.sh --tag=$(git describe --tags --abbrev=0) --increment
+TAG=$(./docroot/profiles/degov/scripts/transform.sh --tag=$(git describe --tags --abbrev=0) --increment)
 git tag ${TAG}
 git push origin ${TAG}
