8db1c138818193227ffa971ccff32128eec1aa39
Sascha Hannes <hannes@publicplan.de>
2018-07-13 08:20:58 +0200

    Updated get tag command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ composer update --prefer-dist
 git add composer.lock
 git commit -m "Updating deGov dependencies automatically"
 git push
-TAG=$(./docroot/profiles/contrib/degov/scripts/transform.sh --tag=$(git describe --tags --abbrev=0) --increment)
+TAG=$(./docroot/profiles/contrib/degov/scripts/transform.sh --tag=$(git describe --tags $(git rev-list --tags --max-count=1)) --increment)
 git tag ${TAG}
 git push origin ${TAG}
