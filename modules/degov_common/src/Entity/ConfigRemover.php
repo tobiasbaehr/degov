a173c6b097d5ecf55ac6ea758f138d2fac75df7c
Sascha Hannes <hannes@publicplan.de>
2018-07-23 17:10:52 +0200

    Removed workbench moderation dependency, clean up workbench moderation stuff

--- modules/degov_common/src/Entity/ConfigRemover.php
+++ modules/degov_common/src/Entity/ConfigRemover.php
@@ -31,29 +31,51 @@ class ConfigRemover {
 
   /**
    * removes a special value from configuration
+   *
    * @param string $configName
    * @param string $configPath
    * @param string $key
    */
- public function removeValueFromConfiguration(string $configName, string $configPath, string $key):void {
-   $this->logger->info("Opening configuration \"$configName.$configPath\"");
+  public function removeValueFromConfiguration(string $configName, string $configPath, string $key): void {
     $config = \Drupal::configFactory()
       ->getEditable($configName);
     $value = $config->get($configPath);
     if ($value && array_key_exists($key, $value)) {
-      $this->logger->info("Removed key \"$key\" of value \"$value\" from configuration \"$configName.$configPath\"");
       unset($value[$key]);
-    }
-    else {
-      $this->logger->info("Skipped configuration \"$configName.$configPath\"");
-    }
-    if ($value) {
       $config->set($configPath, $value);
+      $config->save(TRUE);
     }
-    else {
-      $config->set($configPath, NULL);
-    }
-    $config->save(TRUE);
   }
 
+  /**
+   * removes a special value from configuration
+   *
+   * @param string $configName
+   * @param string $configPath
+   * @param string $key
+   */
+  public function removeListItemFromConfiguration(string $configName, string $configPath, string $key): void {
+    $config = \Drupal::configFactory()
+      ->getEditable($configName);
+    $value = $config->get($configPath);
+    if (!$value) {
+      return;
+    }
+    $value = array_flip($value);
+    if (array_key_exists($key, $value)) {
+      unset($value[$key]);
+      if ($value) {
+        $value = array_keys($value);
+        $config->set($configPath, $value);
+        $config->save(TRUE);
+      }
+      else {
+        //Remove element if it's empty
+        $configParts = explode('.', $configPath);
+        $newKey = array_pop($configParts);
+        $newPath = implode('.', $configParts);
+        $this->removeValueFromConfiguration($configName, $newPath, $newKey);
+      }
+    }
+  }
 }
