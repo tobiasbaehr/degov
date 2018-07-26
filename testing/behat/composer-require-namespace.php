161e2f16015a2761876ff5e0113c3750009f7639
Peter Majmesku <4004ar@gmail.com>
2018-07-26 14:39:48 +0200

    Wrong space

--- testing/behat/composer-require-namespace.php
+++ testing/behat/composer-require-namespace.php
@@ -4,6 +4,6 @@
 $file = 'composer.json';
 $data = json_decode(file_get_contents($file), true);
 $data["autoload-dev"]["psr-4"] = [
-  "Drupal\\degov\\Behat\\Context\\"  => "docroot/profiles/contrib/degov/testing/behat/context/",
+  "Drupal\\degov\\Behat\\Context\\" => "docroot/profiles/contrib/degov/testing/behat/context/",
 ];
 file_put_contents('composer.json', json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT));
