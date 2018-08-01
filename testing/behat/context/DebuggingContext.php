0e6e807dadbf5601666408fd443088732343caaa
Peter Majmesku <4004ar@gmail.com>
2018-08-01 16:21:11 +0200

    Prevent Behat output buffering

--- testing/behat/context/DebuggingContext.php
+++ testing/behat/context/DebuggingContext.php
@@ -10,7 +10,7 @@ class DebuggingContext extends RawMinkContext {
    * @Then /^I print current page HTML markup into pipeline - DEBUG$/
    */
   public function printCurrentPageHTMLMarkup() {
-    var_dump($this->getSession()->getPage()->getHtml());
+    die($this->getSession()->getPage()->getHtml());
   }
 
 }
