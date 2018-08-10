3b1d476891112952cd2f1ea7da1f5494d6706339
Peter Majmesku <majmesku@publicplan.de>
2018-08-10 12:06:45 +0200

    Removed faulty config, re-added void return types

--- testing/behat/context/ModuleContext.php
+++ testing/behat/context/ModuleContext.php
@@ -44,7 +44,7 @@ class ModuleContext extends RawDrupalContext {
   /**
    * @Then /^I am installing the "([^"]*)" module$/
    */
-  public function iAmInstallingTheModule(string $moduleName) {
+  public function iAmInstallingTheModule(string $moduleName): void {
     $this->getModuleInstaller()->install([$moduleName]);
   }
 
@@ -58,7 +58,7 @@ class ModuleContext extends RawDrupalContext {
    *
    * @Given I am installing the following Drupal modules:
    */
-  public function installMultipleDrupalModules(TableNode $modulesTable) {
+  public function installMultipleDrupalModules(TableNode $modulesTable): void {
     $rowsHash = $modulesTable->getRowsHash();
     $moduleMachineNames = array_keys($rowsHash);
 
