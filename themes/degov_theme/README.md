b1c3a20a5346d585fef4a30979c4612ccb2dc351
Peter Majmesku <majmesku@publicplan.de>
2018-08-17 16:52:22 +0200

    Implemented iconset

---
+++ themes/degov_theme/README.md
@@ -0,0 +1,15 @@
+# deGov Theme
+
+## NPM packages
+Run `npm install` to get the dependencies.
+
+## Compile your assets (JS and CSS files)
+
+### Development
+
+You must disable CSS and JS aggregation in your Drupal backend, if you want to compile the assets in development mode.
+
+`npm run-script build`
+
+### Production
+`npm run-script build:prod`
---
+++ themes/degov_theme/webpack.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: [
+    // deGov theme
+    './scss/style.scss',
+    // Bootstrap
+    './node_modules/bootstrap/js/src/carousel.js',
+    './node_modules/bootstrap/scss/bootstrap-grid.scss',
+    // Octicons
+    './node_modules/octicons/index.js',
+    './node_modules/octicons/index.scss',
+  ],
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  output: {
+    path: path.join(__dirname, '/webpack-dist/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules)/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            babelrc: './.babelrc'
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ],
+      }
+    ]
+  }
+};
