c6ff9f20fcd5d0c7c1218f91123ae82dded7d1e7
Peter Majmesku <majmesku@publicplan.de>
2018-08-20 10:30:27 +0200

    Modified README.md. Added latest fontawesome iconset version.

--- themes/degov_theme/README.md
+++ themes/degov_theme/README.md
@@ -1,5 +1,10 @@
 # deGov Theme
 
+## Assets packaging
+
+We are using the [Webpack](https://webpack.js.org/) JS/CSS module bundler. Find its configuration in the `webpack.js`
+file.
+
 ## NPM packages
 Run `npm install` to get the dependencies.
 
@@ -8,8 +13,13 @@ Run `npm install` to get the dependencies.
 ### Development
 
 You must disable CSS and JS aggregation in your Drupal backend, if you want to compile the assets in development mode.
+Enabled CSS and JS aggregation in Drupal works only, if you are compiling the assets in production mode.
 
 `npm run-script build`
 
 ### Production
 `npm run-script build:prod`
+
+## Iconfonts
+
+We are using the Font Awesome free package. See icons palette [here](https://fontawesome.com/icons?d=gallery&m=free).
--- themes/degov_theme/webpack.js
+++ themes/degov_theme/webpack.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -8,9 +9,9 @@ module.exports = {
     // Bootstrap
     './node_modules/bootstrap/js/src/carousel.js',
     './node_modules/bootstrap/scss/bootstrap-grid.scss',
-    // Octicons
-    './node_modules/octicons/index.js',
-    './node_modules/octicons/index.scss',
+    // Font Awesome
+    './node_modules/@fortawesome/fontawesome-free/js/all.js',
+    './node_modules/@fortawesome/fontawesome-free/css/all.css',
   ],
   plugins: [
     new MiniCssExtractPlugin({
@@ -18,7 +19,12 @@ module.exports = {
       // both options are optional
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CopyWebpackPlugin([{
+      context: `${__dirname}/node_modules/@fortawesome/fontawesome-free/webfonts`,
+      from: '*',
+      to: `${__dirname}/webfonts/`
+    }])
   ],
   output: {
     path: path.join(__dirname, '/webpack-dist/'),
@@ -37,7 +43,7 @@ module.exports = {
         }]
       },
       {
-        test: /\.scss$/,
+        test: [/\.scss$/, /\.css$/],
         use: [
           MiniCssExtractPlugin.loader,
           { loader: 'css-loader', options: { url: false, sourceMap: true } },
