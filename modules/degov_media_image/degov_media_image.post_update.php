6a6875ef4afad5127eb48d1decf6a4795fc01a30
Sascha Hannes <hannes@publicplan.de>
2018-09-25 14:22:29 +0200

    Re-Indexed search_api, removed default search, removed old facet

--- modules/degov_media_image/degov_media_image.post_update.php
+++ modules/degov_media_image/degov_media_image.post_update.php
@@ -48,6 +48,12 @@ function degov_media_image_post_update_migrate_field_date(&$sandbox) {
     }
     $sandbox['current']++;
   }
+  if($sandbox['current'] / $sandbox['total'] === 1) {
+    $index = \Drupal\search_api\Entity\Index::load('search_media');
+    if ($index) {
+      $index->reindex();
+    }
+  }
 
   $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
 
