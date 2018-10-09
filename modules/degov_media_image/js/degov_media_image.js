ed19e8ed7586dc29b757e85f394f0bdd080e1d7f
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-09 11:05:10 +0200

    Setting an image as royalty free will empty and disable the copyright field, refactored and extended the tests regarding this behavior

--- modules/degov_media_image/js/degov_media_image.js
+++ modules/degov_media_image/js/degov_media_image.js
@@ -12,15 +12,18 @@
             var required_class = 'form-required';
             var details_container_id = $('[name*=field_copyright][name*=target_id]').closest('details').attr('id');
             var copyright_field = $('[name*=field_copyright][name*=target_id]');
-            var copyright_field_closest_label = copyright_field.closest('.form-item').children('label');
+            var copyright_field_form_item = copyright_field.closest('.form-item');
+            var copyright_field_closest_label = copyright_field_form_item.children('label');
             var vertical_tab = $('.vertical-tabs__menu').find('a[href="#' + details_container_id + '"]').find('.vertical-tabs__menu-item-title');
 
             if($('#edit-field-royalty-free-value:checked').length === 0) {
-                copyright_field.prop('required', 'required').addClass('required');
+                copyright_field.prop('disabled', false).prop('required', 'required').addClass('required');
+                copyright_field_form_item.removeClass('form-disabled');
                 copyright_field_closest_label.addClass(required_class);
                 vertical_tab.addClass(required_class);
             } else {
-                copyright_field.removeProp('required').removeClass('required');
+                copyright_field.val('').prop('required', false).removeClass('required').prop('disabled', 'disabled');
+                copyright_field_form_item.addClass('form-disabled');
                 copyright_field_closest_label.removeClass(required_class);
                 vertical_tab.removeClass(required_class);
             }
