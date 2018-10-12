41b7e4a0b2c2d4fbf68d192bd3877ef08c7dbae8
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-12 13:20:20 +0200

    Revert "Ensure that the royalty free checkbox clears the copyright field when the image form is displayed in a modal dialog"
    This reverts commit 43345f27db1703dfbf3632b14ba08bd983f1097d.

--- modules/degov_media_image/js/degov_media_image.js
+++ modules/degov_media_image/js/degov_media_image.js
@@ -2,22 +2,21 @@
     'use strict';
 
     Drupal.behaviors.degov_media_image = {
-        toggle_field_selector: '[id^=edit-field-royalty-free-value]',
         attach: function (context, settings) {
             Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
-            $(Drupal.behaviors.degov_media_image.toggle_field_selector).on('click', function(){
+            $('#edit-field-royalty-free-value').on('click', function(){
                 Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
             });
         },
         toggle_copyright_required_indicators: function() {
-            let required_class = 'form-required',
-                details_container_id = $('[name*=field_copyright][name*=target_id]').closest('details').attr('id'),
-                copyright_field = $('[name*=field_copyright][name*=target_id]'),
-                copyright_field_form_item = copyright_field.closest('.form-item'),
-                copyright_field_closest_label = copyright_field_form_item.children('label'),
-                vertical_tab = $('.vertical-tabs__menu').find('a[href="#' + details_container_id + '"]').find('.vertical-tabs__menu-item-title');
+            var required_class = 'form-required';
+            var details_container_id = $('[name*=field_copyright][name*=target_id]').closest('details').attr('id');
+            var copyright_field = $('[name*=field_copyright][name*=target_id]');
+            var copyright_field_form_item = copyright_field.closest('.form-item');
+            var copyright_field_closest_label = copyright_field_form_item.children('label');
+            var vertical_tab = $('.vertical-tabs__menu').find('a[href="#' + details_container_id + '"]').find('.vertical-tabs__menu-item-title');
 
-            if($(Drupal.behaviors.degov_media_image.toggle_field_selector + ':checked').length === 0) {
+            if($('#edit-field-royalty-free-value:checked').length === 0) {
                 copyright_field.prop('disabled', false).prop('required', 'required').addClass('required');
                 copyright_field_form_item.removeClass('form-disabled');
                 copyright_field_closest_label.addClass(required_class);
