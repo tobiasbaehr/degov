1847a35751390fc43564291ec7178e3f3fd5491c
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-22 10:11:02 +0200

    Added a drush command to reset the demo content

---
+++ modules/degov_demo_content/src/Command/DegovDemoContentDrushCommands.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\degov_demo_content\Command;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * Defines drush commands to manage the demo content.
+ */
+class DegovDemoContentDrushCommands extends DrushCommands {
+
+  /**
+   * Deletes and regenerates the demo content.
+   *
+   * @option option-name
+   *   Description
+   * @usage degov_demo_content-commandName foo
+   *   Usage description
+   *
+   * @command degov_demo_content:reset
+   * @aliases dcreg
+   */
+  public function resetContent() {
+    \Drupal::service('degov_demo_content.content_generator')->resetMedia();
+    $this->logger()->success(dt('Media reset.'));
+  }
+}
