1214999d6c92af61feac65312a19d2997a1cd04f
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-22 13:30:02 +0200

    Create / remove the upload directory in hook_install() / hook_uninstall(), implemented a generic delete function

--- modules/degov_demo_content/src/Factory/NodeFactory.php
+++ modules/degov_demo_content/src/Factory/NodeFactory.php
@@ -4,6 +4,13 @@ namespace Drupal\degov_demo_content\Factory;
 
 class NodeFactory extends ContentFactory {
 
+  /**
+   * The entity type we are working with.
+   *
+   * @var string
+   */
+  protected $entityType = 'node';
+
   public function __construct() {
     parent::__construct();
   }
