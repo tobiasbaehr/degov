7c0d3a28136d6b8efd2650a7780652f532029219
Sascha Hannes <hannes@publicplan.de>
2018-10-22 15:15:51 +0200

    Added node creation, Updated services and drush commands

--- modules/degov_demo_content/src/Factory/ContentFactory.php
+++ modules/degov_demo_content/src/Factory/ContentFactory.php
@@ -4,6 +4,10 @@ namespace Drupal\degov_demo_content\Factory;
 
 use Symfony\Component\Yaml\Yaml;
 
+define('DEGOV_DEMO_CONTENT_TAGS_VOCABULARY_NAME', 'tags');
+define('DEGOV_DEMO_CONTENT_TAG_NAME', 'degov_demo_content');
+define('DEGOV_DEMO_CONTENT_FILES_SAVE_PATH', 'public://degov_demo_content');
+
 class ContentFactory {
 
   /**
@@ -30,13 +34,13 @@ class ContentFactory {
   }
 
   /**
-   * Looks for a media.yml file and reads the date stored within.
+   * Looks for a file and reads the date stored within.
    */
   protected function loadDefinitions($definitions_file_name) {
-    $media_definitions_file_path = $this->moduleHandler->getModule('degov_demo_content')
+    $definitions_file_path = $this->moduleHandler->getModule('degov_demo_content')
         ->getPath() . '/entity_definitions/' . $definitions_file_name;
-    if (file_exists($media_definitions_file_path) && is_file($media_definitions_file_path) && is_readable($media_definitions_file_path)) {
-      return Yaml::parseFile($media_definitions_file_path);
+    if (file_exists($definitions_file_path) && is_file($definitions_file_path) && is_readable($definitions_file_path)) {
+      return Yaml::parseFile($definitions_file_path);
     }
   }
 
@@ -46,22 +50,24 @@ class ContentFactory {
    */
   public function getDemoContentTagId() {
     $tag_term = taxonomy_term_load_multiple_by_name(DEGOV_DEMO_CONTENT_TAG_NAME, DEGOV_DEMO_CONTENT_TAGS_VOCABULARY_NAME);
-    if(!empty($tag_term)) {
+    if (!empty($tag_term)) {
       $tag_term = reset($tag_term);
       return $tag_term->id();
     }
-    return null;
+    return NULL;
   }
 
   /**
    * Deletes the generated entities.
    */
   public function deleteContent() {
-    $entities = \Drupal::entityTypeManager()->getStorage($this->entityType)->loadByProperties([
-      'field_tags' => $this->getDemoContentTagId(),
-    ]);
+    $entities = \Drupal::entityTypeManager()
+      ->getStorage($this->entityType)
+      ->loadByProperties([
+        'field_tags' => $this->getDemoContentTagId(),
+      ]);
 
-    foreach($entities as $entity) {
+    foreach ($entities as $entity) {
       $entity->delete();
     }
   }
