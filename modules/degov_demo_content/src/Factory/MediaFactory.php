1b5e7ddd623699dee86bf8f3a4caf10e0afdf123
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-24 15:35:58 +0200

    Streamlined media creation a bit

--- modules/degov_demo_content/src/Factory/MediaFactory.php
+++ modules/degov_demo_content/src/Factory/MediaFactory.php
@@ -56,6 +56,7 @@ class MediaFactory extends ContentFactory {
    */
   public function generateContent() {
     $media_to_generate = $this->loadDefinitions('media.yml');
+    $this->prepareValues($media_to_generate);
 
     $this->saveFiles($media_to_generate);
     $this->saveEntities($media_to_generate);
@@ -79,9 +80,11 @@ class MediaFactory extends ContentFactory {
       ->getPath() . '/fixtures';
 
     foreach ($media_to_generate as $media_item_key => $media_item) {
-      $file_data = file_get_contents($fixtures_path . '/' . $media_item['file']);
-      if (($saved_file = file_save_data($file_data, DEGOV_DEMO_CONTENT_FILES_SAVE_PATH . '/' . $media_item['file'], FILE_EXISTS_REPLACE)) !== FALSE) {
-        $this->files[$media_item_key] = $saved_file;
+      if(isset($media_item['file'])) {
+        $file_data = file_get_contents($fixtures_path . '/' . $media_item['file']);
+        if (($saved_file = file_save_data($file_data, DEGOV_DEMO_CONTENT_FILES_SAVE_PATH . '/' . $media_item['file'], FILE_EXISTS_REPLACE)) !== FALSE) {
+          $this->files[$media_item_key] = $saved_file;
+        }
       }
     }
   }
@@ -94,122 +97,66 @@ class MediaFactory extends ContentFactory {
   private function saveEntities($media_to_generate): void {
     // Create the Media entities.
     foreach ($media_to_generate as $media_item_key => $media_item) {
-      $this->prepareValues($media_item);
-
-      if (isset($this->files[$media_item_key])) {
-        $fields = [
-          'bundle'      => $media_item['bundle'],
-          'name'        => $media_item['name'],
-          'field_title' => $media_item['name'],
-          'status'      => $media_item['status'] ?? TRUE,
-          'uid'         => $media_item['user_id'] ?? 1,
-          'field_tags'  => [
-            ['target_id' => $this->getDemoContentTagId()],
-          ],
-        ];
-
-        if(isset($media_item['embed_code'])) {
-          $fields['embed_code'] = $media_item['embed_code'];
+      foreach($media_item as $media_item_field_key => $media_item_field_value) {
+        if($media_item_field_key === 'file') {
+          switch($media_item['bundle']) {
+            case 'image':
+              $fields['image'] = [
+                'target_id' => $this->files[$media_item_key]->id(),
+                'alt'       => $media_item['name'],
+                'title'     => $media_item['name'],
+              ];
+              break;
+            case 'document':
+              $fields['field_document'] = [
+                'target_id' => $this->files[$media_item_key]->id(),
+              ];
+              break;
+            case 'audio':
+              $fields['field_audio_mp3'] = [
+                'target_id' => $this->files[$media_item_key]->id(),
+              ];
+              break;
+            case 'video_upload':
+              $fields['field_video_upload_mp4'] = [
+                'target_id' => $this->files[$media_item_key]->id(),
+              ];
+              break;
+          }
+          continue;
         }
 
-        if(isset($media_item['field_media_publish_date'])) {
-          $fields['field_media_publish_date'] = $media_item['field_media_publish_date'];
-        }
-
-        if (empty($media_item['field_royalty_free'])) {
-          $fields['field_copyright'] = [
-            'target_id' => $this->getDemoContentCopyrightId(),
+        if($media_item_field_key === 'field_address_address') {
+          $fields['field_address_address'] = [
+            $media_item['field_address_address'] ?? [],
           ];
+          continue;
         }
 
-        switch ($media_item['bundle']) {
-          case 'image':
-            $fields['image'] = [
-              'target_id' => $this->files[$media_item_key]->id(),
-              'alt'       => $media_item['name'],
-              'title'     => $media_item['name'],
-            ];
-            $fields['field_image_caption'] = $media_item['field_image_caption'] ?? '';
-            $fields['field_royalty_free'] = $media_item['field_royalty_free'] ?? FALSE;
-            break;
-
-          case 'video_upload':
-            $fields['field_video_upload_mp4'] = [
-              'target_id' => $this->files[$media_item_key]->id(),
-            ];
-            break;
-
-          case 'document':
-            $fields['field_document'] = [
-              'target_id' => $this->files[$media_item_key]->id(),
-            ];
-            break;
-
-          case 'audio':
-            $fields['field_audio_mp3'] = [
-              'target_id' => $this->files[$media_item_key]->id(),
-            ];
-            break;
-
-          case 'address':
-            $fields['field_address_address'] = [
-              $media_item['field_address_address'] ?? [],
-            ];
-            if(!empty($media_item['field_address_location'])) {
-              $fields['field_address_location'] = $this->wktGenerator->wktBuildPoint($media_item['field_address_location']);
-            }
-            $fields['field_address_title'] = $media_item['field_address_title'] ?? '';
-            $fields['field_address_phone'] = $media_item['field_address_phone'] ?? '';
-            $fields['field_address_fax'] = $media_item['field_address_fax'] ?? '';
-            $fields['field_address_email'] = $media_item['field_address_email'] ?? '';
-            break;
-
-          case 'citation':
-            $fields['field_citation_date'] = $media_item['field_citation_date'];
-            $fields['field_citation_text'] = $media_item['field_citation_text'];
-            $fields['field_citation_title'] = $media_item['field_citation_title'];
-            break;
-
-          case 'person':
-            $fields['field_person_info'] = $media_item['field_person_info'];
-            break;
-
-          case 'contact':
-            $fields['field_contact_email'] = $media_item['field_contact_email'];
-            $fields['field_contact_fax'] = $media_item['field_contact_fax'];
-            $fields['field_contact_title'] = $media_item['field_contact_title'];
-            $fields['field_contact_position'] = $media_item['field_contact_position'];
-            $fields['field_contact_tel'] = $media_item['field_contact_tel'];
-            break;
-
-          case 'gallery':
-            $fields['field_description'] = $media_item['field_description'];
-            $fields['field_gallery_title'] = $media_item['field_gallery_title'];
-            $fields['field_subtitle'] = $media_item['field_subtitle'];
-            break;
-
-          case 'some_embed':
-            $fields['field_some_embed_code'] = $media_item['field_some_embed_code'];
-            $fields['field_social_media_source'] = $media_item['field_social_media_source'];
-            break;
-
-          case 'video':
-            $fields['field_description'] = $media_item['field_description'];
-            $fields['field_video_caption'] = $media_item['field_video_caption'];
-            $fields['field_social_media_source'] = $media_item['field_social_media_source'];
-            $fields['field_media_transcription'] = $media_item['field_media_transcription'];
-            $fields['field_subtitle'] = $media_item['field_subtitle'];
-            $fields['field_media_publish_date'] = $media_item['field_media_publish_date'];
-            $fields['field_media_duration'] = $media_item['field_media_duration'];
-            $fields['field_media_video_embed_field'] = $media_item['field_media_video_embed_field'];
-            break;
-
+        if($media_item_field_key === 'field_address_location') {
+          if (!empty($media_item['field_address_location'])) {
+            $fields['field_address_location'] = $this->wktGenerator->wktBuildPoint($media_item['field_address_location']);
+            continue;
+          }
         }
 
-        $new_media = Media::create($fields);
-        $new_media->save();
-        $this->savedEntities[$media_item_key] = $new_media;
+        $fields[$media_item_field_key] = $media_item_field_value;
       }
+
+      $fields['field_title'] = $media_item['name'];
+      $fields['status'] = $media_item['status'] ?? TRUE;
+      $fields['field_tags'] = [
+        ['target_id' => $this->getDemoContentTagId()],
+      ];
+      if (empty($media_item['field_royalty_free'])) {
+        $fields['field_copyright'] = [
+          'target_id' => $this->getDemoContentCopyrightId(),
+        ];
+      }
+
+      $new_media = Media::create($fields);
+      $new_media->save();
+      $this->savedEntities[$media_item_key] = $new_media;
     }
   }
 
@@ -270,8 +217,8 @@ class MediaFactory extends ContentFactory {
           case 'gallery':
             if (!empty($media_item['field_gallery_images'])) {
               $image_target_ids = [];
-              foreach($media_item['field_gallery_images'] as $image_key) {
-                if(isset($this->savedEntities[$image_key])) {
+              foreach ($media_item['field_gallery_images'] as $image_key) {
+                if (isset($this->savedEntities[$image_key])) {
                   $image_target_ids[] = [
                     'target_id' => $this->savedEntities[$image_key]->id(),
                   ];
