52e146eeda72b6f84b3f8b53a45cef97768a6f0e
Marc-Oliver Teschke <teschke@publicplan.de>
2018-10-25 14:34:15 +0200

    Added support for center-offsets to the AutoCropper

--- modules/degov_media_image/src/Service/AutoCropper.php
+++ modules/degov_media_image/src/Service/AutoCropper.php
@@ -7,6 +7,8 @@ use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\FileSystem;
+use Drupal\crop\Entity\CropType;
+use Drupal\file\Entity\File;
 
 /**
  * Class AutoCropper.
@@ -50,7 +52,7 @@ class AutoCropper {
    * @param \Drupal\file\Entity\File $file
    *   The File entity the image crops should be applied to.
    */
-  public function applyImageCrops($file): void {
+  public function applyImageCrops(File $file): void {
     try {
       $crop_types = $this->entityTypeManager
         ->getStorage('crop_type')
@@ -68,7 +70,7 @@ class AutoCropper {
                 'uri'  => $file->getFileUri(),
               ]);
 
-            $measurements = $this->calculateCropDimensions($crop_type, $image_dimensions);
+            $crop_dimensions = $this->calculateCropDimensions($crop_type, $image_dimensions);
 
             if (empty($crop)) {
               $crop_values = [
@@ -76,29 +78,31 @@ class AutoCropper {
                 'entity_id'   => $file->id(),
                 'entity_type' => 'file',
                 'uri'         => $file->getFileUri(),
-                'x'           => $image_dimensions[0] / 2,
-                'y'           => $image_dimensions[1] / 2,
+                'x'           => $crop_dimensions['x'],
+                'y'           => $crop_dimensions['y'],
               ];
-              $crop_values += $measurements;
+              $crop_values += $crop_dimensions;
               $crop = $this->entityTypeManager
                 ->getStorage('crop')
                 ->create($crop_values);
             }
             else {
               $crop = reset($crop);
-              $crop->set('x', $image_dimensions[0] / 2);
-              $crop->set('y', $image_dimensions[1] / 2);
-              $crop->set('width', $measurements['width']);
-              $crop->set('height', $measurements['height']);
+              $crop->set('x', $crop_dimensions['x']);
+              $crop->set('y', $crop_dimensions['y']);
+              $crop->set('width', $crop_dimensions['width']);
+              $crop->set('height', $crop_dimensions['height']);
             }
             $crop->save();
-          } catch (PluginNotFoundException | InvalidPluginDefinitionException | EntityStorageException $exception) {
+          }
+          catch (PluginNotFoundException | InvalidPluginDefinitionException | EntityStorageException $exception) {
             // Crop not found or crop save failed. Log and continue.
             error_log($exception->getMessage());
           }
         }
       }
-    } catch (PluginNotFoundException | InvalidPluginDefinitionException $exception) {
+    }
+    catch (PluginNotFoundException | InvalidPluginDefinitionException $exception) {
       // No crop types found. Just log, otherwise do nothing.
       error_log($exception->getMessage());
     }
@@ -107,7 +111,7 @@ class AutoCropper {
   /**
    * Calculate the height and width of the crop frame to be applied.
    *
-   * @param object $crop_type
+   * @param \Drupal\crop\Entity\CropType $crop_type
    *   The Crop crop_type we want to apply.
    * @param array $image_dimensions
    *   The actual width and height of the image file.
@@ -115,28 +119,58 @@ class AutoCropper {
    * @return array
    *   The desired height and width of the image.
    */
-  private function calculateCropDimensions($crop_type, array $image_dimensions): array {
+  private function calculateCropDimensions(CropType $crop_type, array $image_dimensions): array {
+    $crop_dimensions = [
+      'height' => 0,
+      'width'  => 0,
+      'x'      => 0,
+      'y'      => 0,
+    ];
     $aspect_ratio_fragments = explode(':', $crop_type->aspect_ratio);
     if (count($aspect_ratio_fragments) !== 2) {
       $aspect_ratio_fragments = [1, 1];
     }
 
-    if ($image_dimensions[0] >= $image_dimensions[1]) {
-      // Landscape orientation.
-      $measurements = [
-        'width'  => (($image_dimensions[1] / $aspect_ratio_fragments[1]) * $aspect_ratio_fragments[0]) * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments),
-        'height' => $image_dimensions[1] * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments),
-      ];
+    $orientation = 'squared';
+    if ($image_dimensions[0] > $image_dimensions[1]) {
+      $orientation = 'landscape';
     }
     else {
-      // Portrait orientation.
-      $measurements = [
-        'width'  => $image_dimensions[0] * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments),
-        'height' => (($image_dimensions[0] / $aspect_ratio_fragments[0]) * $aspect_ratio_fragments[1]) * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments),
-      ];
+      if ($image_dimensions[0] < $image_dimensions[1]) {
+        $orientation = 'portrait';
+      }
     }
 
-    return $measurements;
+    switch ($orientation) {
+      case 'landscape':
+        // Landscape orientation.
+        $crop_dimensions['height'] = $image_dimensions[1] * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        $crop_dimensions['width'] = (($image_dimensions[1] / $aspect_ratio_fragments[1]) * $aspect_ratio_fragments[0]) * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        break;
+
+      case 'portrait':
+        // Portrait orientation.
+        $crop_dimensions['height'] = (($image_dimensions[0] / $aspect_ratio_fragments[0]) * $aspect_ratio_fragments[1]) * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        $crop_dimensions['width'] = $image_dimensions[0] * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        break;
+
+      case 'squared':
+        $crop_dimensions['height'] = (($image_dimensions[0] / $aspect_ratio_fragments[0]) * $aspect_ratio_fragments[1]) * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        $crop_dimensions['width'] = $image_dimensions[0] * $this->calculateScaleFactor($image_dimensions, $aspect_ratio_fragments);
+        break;
+    }
+
+    // @TODO Get offsets from the CropType.
+    $offsets = [
+      'left'   => 1,
+      'right'  => 1,
+      'top'    => 1,
+      'bottom' => 1,
+    ];
+
+    $this->calculateCropCenterOffsets($offsets, $image_dimensions, $crop_dimensions);
+
+    return $crop_dimensions;
   }
 
   /**
@@ -163,6 +197,39 @@ class AutoCropper {
       return $image_ratio / $crop_ratio;
     }
     return 1;
+  }
+
+  /**
+   * Calculates the crop center position based on offset values.
+   *
+   * @param array $offsets
+   *   The array of offsets the calculation should be based on.
+   * @param array $image_dimensions
+   *   The dimensions of the current image.
+   * @param array $crop_dimensions
+   *   A reference to the dimensions array, will be populated with the offsets.
+   */
+  private function calculateCropCenterOffsets(array $offsets, array $image_dimensions, array &$crop_dimensions) {
+
+    $crop_dimensions['x'] = $image_dimensions[0] / 2;
+    $crop_dimensions['y'] = $image_dimensions[1] / 2;
+
+    $remaining_space['vertical'] = $image_dimensions[1] - $crop_dimensions['height'];
+    $remaining_space['horizontal'] = $image_dimensions[0] - $crop_dimensions['width'];
+
+    if ($offsets['left'] >= 0 && $offsets['right'] >= 0) {
+      $offsets_sum = $offsets['left'] + $offsets['right'];
+      $required_padding['left'] = ($remaining_space['horizontal'] / $offsets_sum) * $offsets['left'];
+      $required_padding['right'] = ($remaining_space['horizontal'] / $offsets_sum) * $offsets['right'];
+      $crop_dimensions['x'] += ($required_padding['left'] - $required_padding['right']) / 2;
+    }
+
+    if ($offsets['top'] >= 0 && $offsets['bottom'] >= 0) {
+      $offsets_sum = $offsets['top'] + $offsets['bottom'];
+      $required_padding['top'] = ($remaining_space['vertical'] / $offsets_sum) * $offsets['top'];
+      $required_padding['bottom'] = ($remaining_space['vertical'] / $offsets_sum) * $offsets['bottom'];
+      $crop_dimensions['y'] += ($required_padding['top'] - $required_padding['bottom']) / 2;
+    }
   }
 
 }
