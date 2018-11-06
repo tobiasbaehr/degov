0507a9dcbdb783176dc056357e400576e979ee96
Marc-Oliver Teschke <teschke@publicplan.de>
2018-11-06 16:43:06 +0100

    Moved everything into one function that will take the requested attribute as a parameter

--- modules/degov_theming/src/TwigExtension/LinkExtension.php
+++ modules/degov_theming/src/TwigExtension/LinkExtension.php
@@ -15,54 +15,45 @@ class LinkExtension extends \Twig_Extension {
    */
   public function getFilters(): array {
     return [
-      'get_menu_item_target' => new \Twig_Filter_Function([
+      'menu_item_attribute' => new \Twig_Filter_Function([
         $this,
-        'getMenuItemTarget',
-      ]),
-      'get_menu_item_rel'    => new \Twig_Filter_Function([
-        $this,
-        'getMenuItemRel',
+        'getMenuItemAttribute',
       ]),
     ];
   }
 
   /**
-   * Returns the target attribute for a given menu item.
+   * Returns the requested attribute for a given menu item.
+   *
+   * Currently supports @target and @rel.
    *
    * @param array $menu_item
    *   The menu item.
+   * @param string $attribute
+   *   The requested attribute.
    *
    * @return \Twig_Markup
    *   The target attribute, or an empty string if nothing set.
    */
-  public function getMenuItemTarget(array $menu_item): \Twig_Markup {
-    $target = FALSE;
-    if (!empty($menu_item['external']) && (bool) $menu_item['external'] === TRUE) {
-      $target = '_blank';
-    }
-    if (!empty($menu_item['class']['options']['attributes']['target'])) {
-      $target = $menu_item['class']['options']['attributes']['target'];
-    }
-    if ($target) {
-      return new \Twig_Markup(sprintf(' target="%s"', $target), 'UTF-8');
-    }
-    return new \Twig_Markup('', 'UTF-8');
-  }
+  public function getMenuItemAttribute(array $menu_item, string $attribute): \Twig_Markup {
+    $output = '';
+    switch ($attribute) {
+      case 'target':
+        if (!empty($menu_item['external']) && (bool) $menu_item['external'] === TRUE) {
+          $output = 'target="_blank"';
+        }
+        if (!empty($menu_item['class']['options']['attributes']['target'])) {
+          $output = sprintf('target="%s"', $menu_item['class']['options']['attributes']['target']);
+        }
+        break;
 
-  /**
-   * Returns the rel attribute for a given menu item.
-   *
-   * @param array $menu_item
-   *   The menu item.
-   *
-   * @return \Twig_Markup
-   *   The rel attribute, or an empty string if nothing set.
-   */
-  public function getMenuItemRel(array $menu_item): \Twig_Markup {
-    if (!empty($menu_item['class']['options']['attributes']['rel'])) {
-      return new \Twig_Markup(sprintf(' rel="%s"', $menu_item['class']['options']['attributes']['rel']), 'UTF-8');
+      case 'rel':
+        if (!empty($menu_item['class']['options']['attributes']['rel'])) {
+          $output = sprintf(' rel="%s"', $menu_item['class']['options']['attributes']['rel']);
+        }
+        break;
     }
-    return new \Twig_Markup('', 'UTF-8');
+    return new \Twig_Markup($output, 'UTF-8');
   }
 
 }
