69c6fe7b4675c77e6a05ca21c6e1b2d6d17020c0
Marc-Oliver Teschke <teschke@publicplan.de>
2018-11-16 09:33:57 +0100

    Apply the library and validation-function to all forms where an image media entity could be added, rebuilt validation so that we also cover the craziness that is the entity browser form.

--- modules/degov_media_image/js/degov_media_image.js
+++ modules/degov_media_image/js/degov_media_image.js
@@ -1,34 +1,34 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.degov_media_image = {
-        toggle_field_selector: '[id^=edit-field-royalty-free-value]',
-        attach: function (context, settings) {
-            Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
-            $(Drupal.behaviors.degov_media_image.toggle_field_selector).on('click', function(){
-                Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
-            });
-        },
-        toggle_copyright_required_indicators: function() {
-            let required_class = 'form-required',
-                details_container_id = $('[name*=field_copyright][name*=target_id]').closest('details').attr('id'),
-                copyright_field = $('[name*=field_copyright][name*=target_id]'),
-                copyright_field_form_item = copyright_field.closest('.form-item'),
-                copyright_field_closest_label = copyright_field_form_item.children('label'),
-                vertical_tab = $('.vertical-tabs__menu').find('a[href="#' + details_container_id + '"]').find('.vertical-tabs__menu-item-title');
+  Drupal.behaviors.degov_media_image = {
+    toggle_field_selector: '[id*=-field-royalty-free-value]',
+    attach: function (context, settings) {
+      Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
+      $(Drupal.behaviors.degov_media_image.toggle_field_selector).on('click', function () {
+        Drupal.behaviors.degov_media_image.toggle_copyright_required_indicators();
+      });
+    },
+    toggle_copyright_required_indicators: function () {
+      let required_class = 'form-required',
+        details_container_id = $('[name*=field_copyright][name*=target_id]').closest('details').attr('id'),
+        copyright_field = $('[name*=field_copyright][name*=target_id]'),
+        copyright_field_form_item = copyright_field.closest('.form-item'),
+        copyright_field_closest_label = copyright_field_form_item.children('label'),
+        vertical_tab = $('.vertical-tabs__menu').find('a[href="#' + details_container_id + '"]').find('.vertical-tabs__menu-item-title');
 
-            if($(Drupal.behaviors.degov_media_image.toggle_field_selector + ':checked').length === 0) {
-                copyright_field.prop('disabled', false).prop('required', 'required').addClass('required');
-                copyright_field_form_item.removeClass('form-disabled');
-                copyright_field_closest_label.addClass(required_class);
-                vertical_tab.addClass(required_class);
-            } else {
-                copyright_field.val('').prop('required', false).removeClass('required').prop('disabled', 'disabled');
-                copyright_field_form_item.addClass('form-disabled');
-                copyright_field_closest_label.removeClass(required_class);
-                vertical_tab.removeClass(required_class);
-            }
-        }
-    };
+      if ($(Drupal.behaviors.degov_media_image.toggle_field_selector + ':checked').length === 0) {
+        copyright_field.prop('disabled', false).prop('required', 'required').addClass('required');
+        copyright_field_form_item.removeClass('form-disabled');
+        copyright_field_closest_label.addClass(required_class);
+        vertical_tab.addClass(required_class);
+      } else {
+        copyright_field.val('').prop('required', false).removeClass('required').prop('disabled', 'disabled');
+        copyright_field_form_item.addClass('form-disabled');
+        copyright_field_closest_label.removeClass(required_class);
+        vertical_tab.removeClass(required_class);
+      }
+    }
+  };
 
 }(jQuery));
