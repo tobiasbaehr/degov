f9c6d3982ec11573aaadcf3ae3ce666c1fb73b15
Sascha Hannes <hannes@publicplan.de>
2018-11-25 22:11:23 +0100

    Reformatted code

--- modules/degov_common/src/DeGovModuleIntegrity.php
+++ modules/degov_common/src/DeGovModuleIntegrity.php
@@ -11,18 +11,18 @@ class DeGovModuleIntegrity {
 
   public function checkModule($moduleName): array {
     $missingConfiguration = [];
-    if(strpos($moduleName,'degov') === FALSE) return $missingConfiguration;
+    if (strpos($moduleName, 'degov') === FALSE) {
+      return $missingConfiguration;
+    }
     $files = file_scan_directory(drupal_get_path('module', $moduleName) . '/config/install', '/[a-z0-9_]*\.yml/');
     foreach ($files as $file) {
       $fileName = $file->filename;
-      $configName = str_replace('.yml','', $fileName);
+      $configName = str_replace('.yml', '', $fileName);
       if (empty(\Drupal::configFactory()->get($configName)->getRawData())) {
         $missingConfiguration[$moduleName][] = $configName;
       }
     }
-    if(empty($missingConfiguration)) {
-      $missingConfiguration[$moduleName] = (string)t('OK');
-    }
+
     return $missingConfiguration;
   }
 }
