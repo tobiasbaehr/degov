fb317725e197367d84ca6dee5904e68087f0b3ba
Sascha Hannes <hannes@publicplan.de>
2018-11-26 11:16:26 +0100

    Create error (tests pipeline)

--- testing/behat/context/FormContext.php
+++ testing/behat/context/FormContext.php
@@ -243,7 +243,8 @@ class FormContext extends RawMinkContext {
 	 * @When /^I press button with label "([^"]*)" via translated text$/
 	 */
 	public function pressButtonTranslate(string $button) {
-		$this->getSession()->getPage()->pressButton($this->translateString($button));
+
+		$this->getSession()->getPage()->pressButton($this->translatString($button));
 	}
 
 	/**
