803af80bb63f5d33b53db4909d0f9a359223975b
Marc-Oliver Teschke <teschke@publicplan.de>
2018-12-17 12:35:21 +0100

    Rework the speed-based quality switcher to support an array of alternative qualities and multiple video elements

--- modules/degov_media_video_mobile/js/drupal_behavior/check_cellular.js
+++ modules/degov_media_video_mobile/js/drupal_behavior/check_cellular.js
@@ -2,37 +2,41 @@ import Navi from './../navi';
 import SpeedChecker from './../speed_checker';
 
 (function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.checkCellular = {
+    attach: function (context, settings) {
+      let videos = settings.degov_media_video_mobile;
 
-    'use strict';
-    Drupal.behaviors.checkCellular = {
-      attach: function (context, settings) {
-        let videos = settings.degov_media_video_mobile.checkCellular;
-
-        if (videos['video_mobile'] && Drupal.behaviors.checkCellular.check()) {
-          $('#' + videos['id']).replaceWith(function () {
-            return $(this).attr('src', videos['video_mobile']);
-          });
-        }
-      },
-      check: function () {
-        let navi_connection = new Navi(navigator),
-          connection = navi_connection.getConnection(),
-          isCellular,
-          speedChecker = new SpeedChecker(window);
-
-        if (connection) {
-          if (typeof connection.type !== 'undefined') {
-            isCellular = (connection.type === 'cellular');
-          } else if (typeof connection.effectiveType !== 'undefined') {
-              isCellular = (connection.effectiveType !== '4g');
+      if (Drupal.behaviors.checkCellular.isCellular()) {
+        for (let id in videos) {
+          let video = videos[id];
+          if (typeof video['files']['video_mobile'] === 'string') {
+            $('#' + video['id']).replaceWith(function () {
+              return $(this).attr('src', video['files']['video_mobile']);
+            });
           }
-        } else {
-          isCellular = speedChecker.checkSlowLoadTime();
         }
-
-        return isCellular;
       }
+    },
+    isCellular: function () {
+      let navi_connection = new Navi(navigator),
+        connection = navi_connection.getConnection(),
+        isCellular,
+        speedChecker = new SpeedChecker(window);
+
+      if (connection) {
+        if (typeof connection.type !== 'undefined') {
+          isCellular = (connection.type === 'cellular');
+        } else if (typeof connection.effectiveType !== 'undefined') {
+          isCellular = (connection.effectiveType !== '4g');
+        }
+      } else {
+        isCellular = speedChecker.checkSlowLoadTime();
+      }
+
+      return isCellular;
     }
+  }
 })(jQuery, Drupal, drupalSettings);
 
 
