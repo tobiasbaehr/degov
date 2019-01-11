2a670691ef2ca50eb6d75613c7c4d477ee876d86
Marc-Oliver Teschke <teschke@publicplan.de>
2019-01-11 10:24:28 +0100

    Remove unused uses

--- testing/behat/context/Traits/TranslationTrait.php
+++ testing/behat/context/Traits/TranslationTrait.php
@@ -2,18 +2,17 @@
 
 namespace Drupal\degov\Behat\Context\Traits;
 
-use Behat\Mink\Exception\ResponseTextException;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\degov\Behat\Context\Exception\TextNotFoundException;
 
 trait TranslationTrait {
 
-	private $langcode = 'de';
+  private $langcode = 'de';
 
-	public function translateString(string $text): string {
-		$translateableMarkup = new TranslatableMarkup($text, [], []);
-		$translatedString = \Drupal::translation()->translateString($translateableMarkup);
-		return $translatedString;
-	}
+  public function translateString(string $text): string {
+    $translateableMarkup = new TranslatableMarkup($text, [], []);
+    $translatedString = \Drupal::translation()
+      ->translateString($translateableMarkup);
+    return $translatedString;
+  }
 
 }
