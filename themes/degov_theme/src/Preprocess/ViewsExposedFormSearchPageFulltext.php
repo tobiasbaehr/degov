3bf24385d31cb8781df4216426eb254f4c7b1017
Dima Storozhuk <dmutro.storozhuk@gmail.com>
2019-01-23 18:04:05 +0200

    Styles for fill text search.

--- themes/degov_theme/src/Preprocess/ViewsExposedFormSearchPageFulltext.php
+++ themes/degov_theme/src/Preprocess/ViewsExposedFormSearchPageFulltext.php
@@ -15,8 +15,24 @@ class ViewsExposedFormSearchPageFulltext {
     }
     $vars['form']['volltext']['#title_display'] = 'invisible';
     $vars['form']['volltext']['#attributes']['placeholder'] = $this->t('Search term');
-    $vars['form']['volltext']['#field_suffix'] = '<i class="fas fa-search"></i>';
-    $vars['form']['volltext']['#wrapper_attributes']['class'][] = 'd-inline-block';
-    $vars["form"]["actions"]['#attributes']['class'][] = 'd-inline-block';
+    $vars['form']['volltext']['#field_suffix'] = '<i class="fas fa-search d-none d-lg-inline-block d-xl-inline-block d-md-none"></i>';
+
+    $classes = [
+      'px-3',
+      'mr-md-3'
+    ];
+    array_walk($classes, function ($class) use (&$vars) {
+      $vars['form']['volltext']['#wrapper_attributes']['class'][] = $class;
+    });
+
+    $classes = [
+      'btn',
+      'btn-primary',
+      'px-3',
+      'px-lg-5'
+    ];
+    array_walk($classes, function ($class) use (&$vars) {
+      $vars["form"]["actions"]['submit']['#attributes']['class'][] = $class;
+    });
   }
 }
