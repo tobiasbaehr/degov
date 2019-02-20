25ea9357752deca624fa01c5a9af14eeb259747d
Marc-Oliver Teschke <teschke@publicplan.de>
2019-02-20 16:41:04 +0100

    Cleanup in the field mapper class

--- modules/degov_media_file_links/src/Service/MediaFileFieldMapper.php
+++ modules/degov_media_file_links/src/Service/MediaFileFieldMapper.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\degov_media_file_links\Service;
 
-use Drupal\media\Entity\Media;
-
 /**
  * Class MediaFileFieldMapper.
  *
@@ -13,9 +11,15 @@ use Drupal\media\Entity\Media;
  */
 class MediaFileFieldMapper {
 
-  public function __construct() {
-  }
-
+  /**
+   * Accepts a bundle name and returns the main file field for this bundle.
+   *
+   * @param string $bundle
+   *   The machine name of the bundle.
+   *
+   * @return string
+   *   The machine name of the main file field associated with the bundle.
+   */
   public function getFileFieldForBundle(string $bundle): string {
     $fileFieldMappings = $this->getBundleFileFieldMappings();
     if (isset($fileFieldMappings[$bundle])) {
@@ -24,9 +28,19 @@ class MediaFileFieldMapper {
     return FALSE;
   }
 
+  /**
+   * Get a list of bundles and their main file fields.
+   *
+   * Moved this into a separate function because we might eventually load the
+   * pairings from config instead of providing a static array.
+   *
+   * @return array
+   *   The array of bundle / field pairs.
+   */
   private function getBundleFileFieldMappings(): array {
     return [
       'document' => 'field_document',
     ];
   }
+
 }
