87128c393da3e89f9df2451d483121e2874fd5b1
Marc-Oliver Teschke <teschke@publicplan.de>
2019-02-20 17:01:36 +0100

    Added empty test classes

---
+++ modules/degov_media_file_links/tests/src/Kernel/LinkResolutionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\Tests\degov_media_file_links\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Class LinkResolutionTestTest.
+ *
+ * @package Drupal\Tests\degov_media_file_links\Kernel
+ */
+class LinkResolutionTestTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   */
+  public function testSetup(): void {
+  }
+
+}
---
+++ modules/degov_media_file_links/tests/src/Kernel/SuggestionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\Tests\degov_media_file_links\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Class SuggestionsTest.
+ *
+ * @package Drupal\Tests\degov_media_file_links\Kernel
+ */
+class SuggestionsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   */
+  public function testSetup(): void {
+  }
+
+}
