ed88054e7e525c0e66e2d4a898b1728d52a37a36
Sascha Hannes <hannes@publicplan.de>
2019-02-22 17:09:32 +0100

    ...

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -42,12 +42,6 @@ mkdir docroot/sites/default/files/
 chmod 777 -R docroot/sites/default/files/
 echo "### Setting up Behat"
 mv docroot/profiles/contrib/degov/testing/behat/behat-no-drupal.yml .
-echo "### Installing drupal with Behat"
-behat -c behat-no-drupal.yml -vvv
-echo "### Updating translation"
-bin/drush locale-check && bin/drush locale-update && bin/drush cr
-echo "### Running Behat tests"
-mv docroot/profiles/contrib/degov/testing/behat/behat.yml .
 wget -O http://app:80
 echo "####"
 echo "####"
@@ -57,6 +51,12 @@ echo "####"
 echo "####"
 echo "####"
 wget -O http://127.0.0.1:80
+echo "### Installing drupal with Behat"
+behat -c behat-no-drupal.yml -vvv
+echo "### Updating translation"
+bin/drush locale-check && bin/drush locale-update && bin/drush cr
+echo "### Running Behat tests"
+mv docroot/profiles/contrib/degov/testing/behat/behat.yml .
 behat
 echo "### Running Behat smoke tests"
 bin/drush upwd admin admin
