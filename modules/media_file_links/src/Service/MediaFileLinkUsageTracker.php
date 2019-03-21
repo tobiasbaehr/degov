0beee00bd229e0390cc6d2b972bb3a3ac88d8620
Marc-Oliver Teschke <teschke@publicplan.de>
2019-03-21 14:48:04 +0100

    Added the schema for the table that will store our tracked references.

--- modules/media_file_links/src/Service/MediaFileLinkUsageTracker.php
+++ modules/media_file_links/src/Service/MediaFileLinkUsageTracker.php
@@ -14,39 +14,46 @@ use Drupal\paragraphs\ParagraphInterface;
  */
 class MediaFileLinkUsageTracker {
 
+  private $placeholderHandler;
+
+  public function __construct(MediaFileLinkPlaceholderHandler $placeholder_handler) {
+    $this->placeholderHandler = $placeholder_handler;
+  }
+
   public function trackMediaUsage(EntityInterface $entity): void {
-    if($entity instanceof NodeInterface) {
+    if ($entity instanceof NodeInterface) {
       $this->trackMediaUsageInNode($entity);
     }
 
-    if($entity instanceof MenuLinkContentInterface) {
+    if ($entity instanceof MenuLinkContentInterface) {
       $this->trackMediaUsageInMenuLinkContent($entity);
     }
 
-    if($entity instanceof ParagraphInterface) {
+    if ($entity instanceof ParagraphInterface) {
       $this->trackMediaUsageInParagraph($entity);
     }
   }
 
   private function trackMediaUsageInNode(NodeInterface $node): void {
-//    foreach($node->getFields() as $field) {
-//      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
-//        'referencing_entity_id' => $node->id(),
-//        'referencing_entity_type' => 'node',
-//        'referencing_entity_langcode' => $node->get('langcode')->getString(),
-//        'media_entity_id' => $field->getString(),
-//      ]);
-//    }
+    //    foreach($node->getFields() as $field) {
+    //      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
+    //        'referencing_entity_id' => $node->id(),
+    //        'referencing_entity_type' => 'node',
+    //        'referencing_entity_langcode' => $node->get('langcode')->getString(),
+    //        'media_entity_id' => $field->getString(),
+    //      ]);
+    //    }
   }
 
   private function trackMediaUsageInMenuLinkContent(MenuLinkContentInterface $menuLinkContent): void {
     $linkValue = $menuLinkContent->get('link')->getValue();
-    if(!empty($linkValue[0]['uri'])) {
-      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
-        'referencing_entity_id' => $menuLinkContent->id(),
-        'referencing_entity_type' => 'menu_link_content',
+    if (!empty($linkValue[0]['uri']) && $this->placeholderHandler->isValidMediaFileLinkPlaceholder($linkValue[0]['uri'])) {
+      $this->storeUsage([
+        'referencing_entity_id'       => $menuLinkContent->id(),
+        'referencing_entity_type'     => 'menu_link_content',
+        'referencing_entity_field'    => 'link',
         'referencing_entity_langcode' => $menuLinkContent->get('langcode')->getString(),
-        'media_entity_id' => $linkValue[0]['uri'],
+        'media_entity_id'             => $this->placeholderHandler->getMediaIdFromPlaceholder($linkValue[0]['uri']),
       ]);
     }
   }
@@ -63,7 +70,8 @@ class MediaFileLinkUsageTracker {
   }
 
   private function storeUsage(array $values): void {
-    //    error_log(print_r($values, 1));
+    error_log(print_r($values, 1));
+    \Drupal::database()->insert('media_file_links_usage')->fields($values)->execute();
   }
 
 }
