e6745d8a7b3280b436c2322b62fdcbd90d933e53
Marc-Oliver Teschke <teschke@publicplan.de>
2019-03-21 13:51:24 +0100

    Added a basic tracker service and some hooks that use it when entities are updated

---
+++ modules/media_file_links/src/Service/MediaFileLinkUsageTracker.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\media_file_links\Service;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\menu_link_content\MenuLinkContentInterface;
+use Drupal\node\NodeInterface;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Class MediaFileLinkUsageTracker.
+ *
+ * @package Drupal\media_file_links\Service
+ */
+class MediaFileLinkUsageTracker {
+
+  public function trackMediaUsage(EntityInterface $entity): void {
+    if($entity instanceof NodeInterface) {
+      $this->trackMediaUsageInNode($entity);
+    }
+
+    if($entity instanceof MenuLinkContentInterface) {
+      $this->trackMediaUsageInMenuLinkContent($entity);
+    }
+
+    if($entity instanceof ParagraphInterface) {
+      $this->trackMediaUsageInParagraph($entity);
+    }
+  }
+
+  private function trackMediaUsageInNode(NodeInterface $node): void {
+//    foreach($node->getFields() as $field) {
+//      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
+//        'referencing_entity_id' => $node->id(),
+//        'referencing_entity_type' => 'node',
+//        'referencing_entity_langcode' => $node->get('langcode')->getString(),
+//        'media_entity_id' => $field->getString(),
+//      ]);
+//    }
+  }
+
+  private function trackMediaUsageInMenuLinkContent(MenuLinkContentInterface $menuLinkContent): void {
+    $linkValue = $menuLinkContent->get('link')->getValue();
+    if(!empty($linkValue[0]['uri'])) {
+      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
+        'referencing_entity_id' => $menuLinkContent->id(),
+        'referencing_entity_type' => 'menu_link_content',
+        'referencing_entity_langcode' => $menuLinkContent->get('langcode')->getString(),
+        'media_entity_id' => $linkValue[0]['uri'],
+      ]);
+    }
+  }
+
+  private function trackMediaUsageInParagraph(ParagraphInterface $paragraph): void {
+    //    foreach($node->getFields() as $field) {
+    //      \Drupal::service('media_file_links.usage_tracker')->trackUsage([
+    //        'referencing_entity_id' => $node->id(),
+    //        'referencing_entity_type' => 'node',
+    //        'referencing_entity_langcode' => $node->get('langcode')->getString(),
+    //        'media_entity_id' => $field->getString(),
+    //      ]);
+    //    }
+  }
+
+  private function storeUsage(array $values): void {
+    //    error_log(print_r($values, 1));
+  }
+
+}
