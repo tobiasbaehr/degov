3e944f93faaa27bf6bdddaf224847fc05fe42b58
Marc-Oliver Teschke <teschke@publicplan.de>
2019-03-29 15:36:08 +0100

    Fix merge conflicts

--- modules/degov_demo_content/src/Generator/GeneratorInterface.php
+++ modules/degov_demo_content/src/Generator/GeneratorInterface.php
@@ -2,12 +2,26 @@
 
 namespace Drupal\degov_demo_content\Generator;
 
+/**
+ * Interface GeneratorInterface
+ *
+ * @package Drupal\degov_demo_content\Generator
+ */
 interface GeneratorInterface {
 
+  /**
+   * Deletes, then regenerates demo content.
+   */
   public function resetContent(): void;
 
+  /**
+   * Generates content from a definitions file.
+   */
   public function generateContent(): void;
 
+  /**
+   * Deletes the generated content.
+   */
   public function deleteContent(): void;
 
 }
