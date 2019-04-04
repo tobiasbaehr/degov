1020ef0d4f88bbe14d76dc2526bdb7ed0537d999
Peter Majmesku <majmesku@publicplan.de>
2019-04-04 14:44:04 +0200

    Re-added old pipeline. Installation of mysql-client.

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
+apt-install mysql-client
 set -e
-
-PHPVERSION=7.2
-
+PHPVERSION=$1
 echo "### Executing Pipeline script with PHP: $PHPVERSION"
 echo "### Setting up project folder"
+
+echo "### Wait for packagist"
+doWhile="0"
+while [ $doWhile -eq "0" ]; do
+   GREP=`wget -q -O - https://packagist.org/packages/degov/degov | grep ">dev-$BITBUCKET_BRANCH<"`
+   if [ ! -z "$GREP" ]; then
+        doWhile=1
+   fi
+   sleep 1
+done
+
 composer create-project degov/degov-project --no-install
 cd degov-project
 rm composer.lock
 composer require "degov/degov:dev-$BITBUCKET_BRANCH#$BITBUCKET_COMMIT" weitzman/drupal-test-traits:1.0.0-alpha.1 --update-with-dependencies
-
+echo "### Starting chrome container"
+docker run -d --name="testing" -p 4444:4444 --net="host" -v "$BITBUCKET_CLONE_DIR/degov-project/docroot/profiles/contrib/degov/testing/fixtures:/home/headless/" -v $BITBUCKET_CLONE_DIR:$BITBUCKET_CLONE_DIR derh4nnes/selenium-chrome-headless
 echo "Setting up project"
 cp docroot/profiles/contrib/degov/testing/behat/composer-require-namespace.php .
 php composer-require-namespace.php
 composer dump-autoload
 echo "### Configuring PHP"
-(cd docroot && php -c /etc/php/$PHPVERSION/cli/php_more_upload.ini -S 0.0.0.0:80 .ht.router.php 2>/dev/null&)
+(cd docroot && screen -dmS php-server php -c /etc/php/$PHPVERSION/cli/php_more_upload.ini -S localhost:80 .ht.router.php)
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 echo "### Checking code standards"
 phpstan analyse docroot/profiles/contrib/degov -c docroot/profiles/contrib/degov/phpstan.neon --level=1 || true
@@ -26,26 +37,20 @@ cp docroot/profiles/contrib/degov/testing/behat/template/settings.local.php docr
 sed -i 's/{{ mysql_auth.db }}/testing/g' docroot/sites/default/settings.local.php
 sed -i 's/{{ mysql_auth.user }}/root/g' docroot/sites/default/settings.local.php
 sed -i 's/{{ mysql_auth.password }}/testing/g' docroot/sites/default/settings.local.php
-sed -i 's/{{ mysql_host }}/mysql/g' docroot/sites/default/settings.local.php
+sed -i 's/localhost/127.0.0.1/g' docroot/sites/default/settings.local.php
 echo '$settings["file_private_path"] = "sites/default/files/private";' >> docroot/sites/default/settings.local.php
 mkdir docroot/sites/default/files/
 chmod 777 -R docroot/sites/default/files/
 echo "### Setting up Behat"
-mv docroot/profiles/contrib/degov/testing/behat/behat.yml .
+mv docroot/profiles/contrib/degov/testing/behat/behat-no-drupal.yml .
 echo "### Installing drupal with Behat"
-behat --suite=no-drupal --strict
-mkdir /app || true
-bin/drush sql-dump --gzip --result-file=/app/dump.sql
-
+behat -c behat-no-drupal.yml -vvv
 echo "### Updating translation"
 bin/drush locale-check && bin/drush locale-update && bin/drush cr
-
 echo "### Running Behat tests"
-behat --suite=default --strict
-
+mv docroot/profiles/contrib/degov/testing/behat/behat.yml .
+behat
 echo "### Running Behat smoke tests"
 bin/drush upwd admin admin
-behat --suite=smoke-tests --strict
-
-# echo "### Running Behat upload tests"
-# behat --suite=tests-with-file-upload --strict
+mv docroot/profiles/contrib/degov/testing/behat/behat-smoke-tests.yml .
+behat -c behat-smoke-tests.yml
