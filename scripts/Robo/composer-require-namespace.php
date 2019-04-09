e192e1fbbe55df1002340e24231ce1f737906009
Peter Majmesku <majmesku@publicplan.de>
2019-04-09 14:58:08 +0200

    Fix Behat namespace

--- scripts/Robo/composer-require-namespace.php
+++ scripts/Robo/composer-require-namespace.php
@@ -3,7 +3,7 @@
 
 $file = 'composer.json';
 $data = json_decode(file_get_contents($file), TRUE);
-$data['autoload-dev']['psr-4'] = [
+$data['autoload-dev']['psr-4'] += [
   'degov\\Scripts\\Robo\\' => 'docroot/profiles/contrib/degov/scripts/Robo'
 ];
 file_put_contents('composer.json', json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
