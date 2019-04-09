3d912e17be1d5b6127f305dd9ad1a1669c8504a6
Peter Majmesku <majmesku@publicplan.de>
2019-04-09 13:45:00 +0200

    Check nodejs version via utility method

--- scripts/Robo/Test/UtilitiesTest.php
+++ scripts/Robo/Test/UtilitiesTest.php
@@ -23,4 +23,35 @@ EOT;
     self::assertEquals('Somewhat with a line break.', Utilities::removeCliLineBreaks($text));
   }
 
+  /**
+   * @dataProvider wrongNodeVersionDataProvider
+   * @expectedException \degov\Scripts\Robo\Exception\ApplicationRequirementFail
+   */
+  public function testCheckApplicationRequirementFail(string $wrongVersion): void {
+    Utilities::checkNodeVersion($wrongVersion);
+  }
+
+  /**
+   * @dataProvider correctNodeVersionDataProvider
+   */
+  public function testCheckNoApplicationRequirementFail(string $wrongVersion): void {
+    self::assertInternalType('null', Utilities::checkNodeVersion($wrongVersion));
+  }
+
+  public function wrongNodeVersionDataProvider(): array {
+    return [
+      ['v9.11.1'],
+      ['9.11.1'],
+      ['blub9.11.1'],
+    ];
+  }
+
+  public function correctNodeVersionDataProvider(): array {
+    return [
+      ['v6.22.1'],
+      ['6.34.5'],
+      ['blub6.34.5'],
+    ];
+  }
+
 }
--- scripts/Robo/Utilities.php
+++ scripts/Robo/Utilities.php
@@ -2,11 +2,27 @@
 
 namespace degov\Scripts\Robo;
 
+use degov\Scripts\Robo\Exception\ApplicationRequirementFail;
 
 class Utilities {
 
   public static function removeCliLineBreaks(string $output): string {
     return str_replace(PHP_EOL, '', $output);
+  }
+
+  /**
+   * @param string $output
+   *  The output of "node -v" cli command.
+   *
+   * @return bool
+   * @throws ApplicationRequirementFail
+   */
+  public static function checkNodeVersion(string $output): void {
+    $onlyVersionNumber = preg_replace('/[^0-9]/', '', $output);
+
+    if (!(strpos($onlyVersionNumber, '6') === 0)) {
+      throw new ApplicationRequirementFail('Aborting. Try again after fixing the NodeJS version requirement');
+    }
   }
 
 }
