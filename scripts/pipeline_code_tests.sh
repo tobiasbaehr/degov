8b28c94f885ff50b3f3cd2afe930efcf65099358
Peter Majmesku <majmesku@publicplan.de>
2019-04-11 09:59:05 +0200

    Parallel testing with and without database dump

---
+++ scripts/pipeline_code_tests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+PHPVERSION=$1
+
+echo "### Executing Pipeline script with PHP: $PHPVERSION"
+echo "### Setting up project folder"
+
+echo "### Wait for packagist"
+doWhile="0"
+while [ $doWhile -eq "0" ]; do
+   GREP=`wget -q -O - https://packagist.org/packages/degov/degov | grep ">dev-$BITBUCKET_BRANCH<"`
+   if [ ! -z "$GREP" ]; then
+        doWhile=1
+   fi
+   sleep 1
+done
+
+composer create-project degov/degov-project --no-install
+cd degov-project
+rm composer.lock
+composer require "degov/degov:dev-$BITBUCKET_BRANCH#$BITBUCKET_COMMIT" weitzman/drupal-test-traits:1.0.0-alpha.1 --update-with-dependencies
+echo "### Starting chrome container"
+docker run -d --name="testing" -p 4444:4444 --net="host" -v "$BITBUCKET_CLONE_DIR/degov-project/docroot/profiles/contrib/degov/testing/fixtures:/home/headless/" -v $BITBUCKET_CLONE_DIR:$BITBUCKET_CLONE_DIR derh4nnes/selenium-chrome-headless
+echo "Setting up project"
+cp docroot/profiles/contrib/degov/testing/behat/composer-require-namespace.php .
+php composer-require-namespace.php
+composer dump-autoload
+echo "### Configuring PHP"
+(cd docroot && screen -dmS php-server php -c /etc/php/7.1/cli/php_more_upload.ini -S localhost:80 .ht.router.php)
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+echo "### Checking code standards"
+phpstan analyse docroot/profiles/contrib/degov -c docroot/profiles/contrib/degov/phpstan.neon --level=1 || true
+echo "### Running PHPUnit and KernelBase tests"
+(cd docroot/profiles/contrib/degov && phpunit --testdox)
