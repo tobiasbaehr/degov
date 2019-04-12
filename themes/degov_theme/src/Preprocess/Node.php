981d9178cf7c59ae35d48eefa8edb9c30f5aa26a
Dima Storozhuk <dmutro.storozhuk@gmail.com>
2019-04-12 10:15:02 +0300

    Add special treatment for media_reference with video_mobile Media.

--- themes/degov_theme/src/Preprocess/Node.php
+++ themes/degov_theme/src/Preprocess/Node.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * @file MediaGallery.php.
- */
 
 namespace Drupal\degov_theme\Preprocess;
 
@@ -17,7 +14,7 @@ class Node {
    *
    * @param array $variables
    */
-  static public function preprocess(array &$variables): void {
+  public function preprocess(array &$variables): void {
     /** @var \Drupal\node\Entity\Node $node */
     $node = $variables['node'];
     // Add created time to the search teaser template.
@@ -27,20 +24,10 @@ class Node {
         ->format($node->getCreatedTime(), 'custom', 'd.m.Y');
     }
 
-    // The configuration for "event" content type doens't use the responsive
+    // The configuration for "event" content type doesn't use the responsive
     // image we need in our teaser. Set it here.
     if ($node->bundle() === 'event') {
-      $responsive_image_style_id = '';
-      if (in_array($variables['view_mode'], [
-        'long_text',
-        'preview',
-        'small_image',
-      ])) {
-        $responsive_image_style_id = 'teaser_squared';
-      }
-      elseif ($variables['view_mode']  === 'slim') {
-        $responsive_image_style_id = 'teaser_landscape';
-      }
+      $responsive_image_style_id = $this->determineResponsiveImageStyle($variables['view_mode']);
       if ($medias = $node->get('field_teaser_image')->referencedEntities()) {
         /** @var \Drupal\media\Entity\Media $media */
         $media = reset($medias);
@@ -51,6 +38,60 @@ class Node {
         ];
       }
     }
+
+    // Special treatment for media_reference with video_mobile Media.
+    if ($node->hasField('field_content_paragraphs')
+      && !$node->field_content_paragraphs->isEmpty()) {
+      $responsive_image_style_id = $this->determineResponsiveImageStyle($variables['view_mode']);
+      $paragraphs = $node->get('field_content_paragraphs')->referencedEntities();
+      if (is_array($paragraphs) && count($paragraphs)) {
+        /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
+        foreach ($paragraphs as $paragraph) {
+          if ($paragraph->bundle() === 'media_reference') {
+            if (!$paragraph->get('field_media_reference_media')->isEmpty()) {
+              /** @var \Drupal\media\Entity\Media $media */
+              $video_media = $paragraph->get('field_media_reference_media')->entity;
+              if ($video_media->bundle() === 'video_mobile') {
+                if (!$video_media->get('field_video_mobile_preview')->isEmpty()) {
+                  /** @var \Drupal\media\Entity\Media $image_media */
+                  $image_media = $video_media->get('field_video_mobile_preview')->entity;
+                  $variables['content']['field_teaser_image'] = [
+                    '#type' => 'responsive_image',
+                    '#responsive_image_style_id' => $responsive_image_style_id,
+                    '#uri' => $image_media->image->entity->getFileUri(),
+                  ];
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Helper function to determine which image style to use.
+   *
+   * Determine responsive image style for particular view mode.
+   *
+   * @param string $view_mode_name
+   *   The view mode to test.
+   *
+   * @return string
+   */
+  protected function determineResponsiveImageStyle(string $view_mode_name): string {
+    $responsive_image_style_id = '';
+    if (in_array($view_mode_name, [
+      'long_text',
+      'preview',
+      'small_image',
+    ])) {
+      $responsive_image_style_id = 'teaser_squared';
+    }
+    elseif ($view_mode_name === 'slim') {
+      $responsive_image_style_id = 'teaser_landscape';
+    }
+    return $responsive_image_style_id;
   }
 
 }
