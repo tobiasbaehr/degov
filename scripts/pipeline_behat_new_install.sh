76ac097a087dcb92be2a8217298a663a15c8e979
Peter Majmesku <majmesku@publicplan.de>
2019-05-10 10:39:29 +0200

    Removed screen install. Do not run phpunit in Behat test.

--- scripts/pipeline_behat_new_install.sh
+++ scripts/pipeline_behat_new_install.sh
@@ -26,10 +26,6 @@ composer dump-autoload
 echo "### Configuring PHP"
 (cd docroot && screen -dmS php-server php -c /etc/php/7.1/cli/php_more_upload.ini -S localhost:80 .ht.router.php)
 export PATH="$HOME/.composer/vendor/bin:$PATH"
-echo "### Checking code standards"
-phpstan analyse docroot/profiles/contrib/degov -c docroot/profiles/contrib/degov/phpstan.neon --level=1 || true
-echo "### Running PHPUnit and KernelBase tests"
-(cd docroot/profiles/contrib/degov && phpunit --testdox)
 echo "### Configuring drupal"
 cp docroot/profiles/contrib/degov/testing/behat/template/settings.local.php docroot/sites/default/settings.local.php
 sed -i 's/{{ mysql_auth.db }}/testing/g' docroot/sites/default/settings.local.php
