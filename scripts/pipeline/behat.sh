b2cd6263babb9a32cca070aec1822664db790578
Peter Majmesku <majmesku@publicplan.de>
2019-05-20 00:31:53 +0200

    Do not set install profile by default

--- scripts/pipeline/behat.sh
+++ scripts/pipeline/behat.sh
@@ -52,6 +52,7 @@ if [[ "$2" == "new_install" ]]; then
 fi
 
 if [[ "$2" == "db_dump" ]]; then
+    echo '$settings["install_profile"] = "degov";' >> docroot/sites/default/settings.local.php
     echo '$settings["hash_salt"] = "7asdiugasd8f623gjwgasgf7a8stfasjdfsdafasdfasdfasdf";' >> docroot/sites/default/settings.local.php
     echo "### Importing db dump"
     zcat docroot/profiles/contrib/degov/testing/behat/degov-7.x-dev.sql.gz | bin/drush sql:cli
