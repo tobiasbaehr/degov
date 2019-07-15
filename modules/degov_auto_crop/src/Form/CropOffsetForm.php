5b58bf632bc3465b76511a5ff64bba07d3982955
Tobias Bähr <tobias.baehr@gzevd.de>
2019-07-15 13:10:30 +0000

    Merged in feature/DEGOV-653-degov_auto_crop-translatable (pull request #503)
    Added translations for degov_auto_crop.
    
    * Make crop types be default engglish and translate them. Added translations for degov_auto_crop.
    
    * Fix typo.
    
    * Changed wording of some translations
    
    Approved-by: Marc-Oliver Teschke <teschke@publicplan.de>

--- modules/degov_auto_crop/src/Form/CropOffsetForm.php
+++ modules/degov_auto_crop/src/Form/CropOffsetForm.php
@@ -28,18 +28,18 @@ class CropOffsetForm extends CropTypeForm {
     ];
     $form['offsets']['landscape'] = [
       '#type'        => 'details',
-      '#title'       => t('Automatic offset (landscape images)'),
-      '#description' => t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
+      '#title'       => $this->t('Automatic offset (landscape images)'),
+      '#description' => $this->t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
     ];
     $form['offsets']['portrait'] = [
       '#type'        => 'details',
-      '#title'       => t('Automatic offset (portrait images)'),
-      '#description' => t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
+      '#title'       => $this->t('Automatic offset (portrait images)'),
+      '#description' => $this->t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
     ];
     $form['offsets']['square'] = [
       '#type'        => 'details',
-      '#title'       => t('Automatic offset (square images)'),
-      '#description' => t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
+      '#title'       => $this->t('Automatic offset (square images)'),
+      '#description' => $this->t('<p>On first upload of a file the crop frame will be placed automatically. Use these values to define offsets for the placement.</p><p>Example: if <code>top</code> is <code>2</code> and <code>bottom</code> is <code>1</code>, the crop will be placed twice as far from the top of the image as from the bottom.</p>'),
     ];
 
     $crop_type = $this->getEntity();
@@ -49,25 +49,25 @@ class CropOffsetForm extends CropTypeForm {
       if (strpos($image_type, '#') === FALSE) {
         $form['offsets'][$image_type]['top'] = [
           '#type'          => 'textfield',
-          '#title'         => t('Offset top'),
+          '#title'         => $this->t('Offset top'),
           '#required'      => TRUE,
           '#default_value' => $stored_offsets[$image_type]['top'] ?? 1,
         ];
         $form['offsets'][$image_type]['bottom'] = [
           '#type'          => 'textfield',
-          '#title'         => t('Offset bottom'),
+          '#title'         => $this->t('Offset bottom'),
           '#required'      => TRUE,
           '#default_value' => $stored_offsets[$image_type]['bottom'] ?? 1,
         ];
         $form['offsets'][$image_type]['left'] = [
           '#type'          => 'textfield',
-          '#title'         => t('Offset left'),
+          '#title'         => $this->t('Offset left'),
           '#required'      => TRUE,
           '#default_value' => $stored_offsets[$image_type]['left'] ?? 1,
         ];
         $form['offsets'][$image_type]['right'] = [
           '#type'          => 'textfield',
-          '#title'         => t('Offset right'),
+          '#title'         => $this->t('Offset right'),
           '#required'      => TRUE,
           '#default_value' => $stored_offsets[$image_type]['right'] ?? 1,
         ];
