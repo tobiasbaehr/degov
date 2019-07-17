6f63abdbc765a2680aac905f8dc8caf44250ba25
Nicky Vandevoorde <nicky.vandevoorde@gmail.com>
2019-07-17 12:01:44 +0000

    Merged in fix/DEGOV-710_non-empty-twig-filter (pull request #501)
    Fixes DEGOV-710 adds non empty twig filter and tests
    
    * Fixes DEGOV-710 adds non empty twig filter and tests
    
    * Fixes DEGOV-710 changes require file in test
    
    * Reworked the Twig extension to suite to the test content and removed weird file requiring of dependencies.
    
    * Check for interpreted and not interpreted strings

---
+++ modules/degov_theming/src/TwigExtension/IsNotEmptyValueExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\degov_theming\TwigExtension;
+
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Template\TwigExtension;
+use Drupal\Core\Theme\ThemeManagerInterface;
+use Twig\TwigFunction;
+
+
+class IsNotEmptyValueExtension extends TwigExtension {
+
+  protected $twigExtensionService;
+
+  protected $logger;
+
+  use EmptyValueCheckerTrait;
+
+  public function __construct(RendererInterface $renderer, UrlGeneratorInterface $url_generator, ThemeManagerInterface $theme_manager, DateFormatterInterface $date_formatter, LoggerChannelFactoryInterface $loggerFactory) {
+    parent::__construct($renderer,  $url_generator,  $theme_manager,  $date_formatter);
+    $this->logger = $loggerFactory->get('degov_theme');
+  }
+
+  public function getFunctions(): array {
+    return [
+      new TwigFunction('is_not_empty', [$this, 'isNotEmpty']),
+    ];
+  }
+
+  public function getName(): string {
+    return 'is_not_empty';
+  }
+
+}
