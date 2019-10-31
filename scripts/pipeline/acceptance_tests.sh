ad034d7498b06c965e8f2de9b88ffdd8f005a027
Marc-Oliver Teschke <teschke@publicplan.de>
2019-10-31 16:33:39 +0100

    Use the most recent Behat Drupal extension in the pipeline

--- scripts/pipeline/acceptance_tests.sh
+++ scripts/pipeline/acceptance_tests.sh
@@ -72,6 +72,7 @@ cd degov-project
 rm composer.lock
 _info "### Install profile"
 _composer require --no-progress "degov/degov:dev-$BITBUCKET_BRANCH#$BITBUCKET_COMMIT" drupal/error_log --update-with-all-dependencies
+_composer require "drupal/drupal-extension:^4.0"
 
 PATH="$(pwd)/bin/:$PATH"
 export PATH
