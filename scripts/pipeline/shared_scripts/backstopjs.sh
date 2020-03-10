92a7c777d14c1eed3eb2a622351e43425b347408
tobiasb <tobias.baehr@gzevd.de>
2020-03-10 18:47:09 +0100

    Check that directory exists.

--- scripts/pipeline/shared_scripts/backstopjs.sh
+++ scripts/pipeline/shared_scripts/backstopjs.sh
@@ -22,6 +22,10 @@ main() {
   cd "$BITBUCKET_CLONE_DIR/project" && \
   _composer remove --dev --no-progress "$GIT_LFS_PROJECT"
   _composer require --dev --no-progress --dev "$GIT_LFS_PROJECT:dev-$GIT_LFS_BRANCH")
+  if [[ ! -d $TESTDIR/lfs_data/bitmaps_reference ]];then
+    _err "Could not load $GIT_LFS_PROJECT:dev-$GIT_LFS_BRANCH. Exit."
+    exit 1
+  fi
   _info "### Load Admin cookie for BackstopJS test"
   _robo degov:create-admin-cookie
   _info "### Running BackstopJS test"
