881f9e2509f6d91abcf26d51d09fb874141a89a2
Marc-Oliver Teschke <teschke@publicplan.de>
2020-03-23 08:23:57 +0000

    Merged in feature/degov-1240-update-demo-content (pull request #705)
    Feature/degov 1240 update demo content
    
    * Added a transcription to the demo audio file
    
    * Did a lot of work to support generating paragraphs for media entities as well just so we can add one subtitle file
    
    * Use LFS and project dev branches
    
    * Merged in feature/degov-1250-drupal-core-884 (pull request #710)
    
    Feature/degov 1250 drupal core 884
    
    * Added Behat scenario checking subtitle files
    
    * Merge branch 'release/8.0.x-dev' into feature/degov-1240-update-demo-content
    
    * Tab titles might not be translated
    
    * Use the better docker image for the pipeline
    
    Approved-by: Peter Majmesku

--- modules/degov_demo_content/src/Generator/NodeGenerator.php
+++ modules/degov_demo_content/src/Generator/NodeGenerator.php
@@ -4,10 +4,8 @@ namespace Drupal\degov_demo_content\Generator;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandler;
-use Drupal\media\Entity\Media;
-use Drupal\media\MediaInterface;
+use Drupal\degov_demo_content\FileHandler\ParagraphsFileHandler;
 use Drupal\node\Entity\Node;
-use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\pathauto\AliasCleanerInterface;
 use Drupal\pathauto\PathautoState;
 
@@ -26,19 +24,29 @@ class NodeGenerator extends ContentGenerator implements GeneratorInterface {
   protected $aliasCleaner;
 
   /**
+   * The paragraphs file handler.
+   *
+   * @var \Drupal\degov_demo_content\FileHandler\ParagraphsFileHandler
+   */
+  protected $paragraphsFileHandler;
+
+  /**
    * NodeGenerator constructor.
    *
    * @param \Drupal\Core\Extension\ModuleHandler $moduleHandler
    *   Module handler.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   Entity type manager.
+   * @param \Drupal\degov_demo_content\FileHandler\ParagraphsFileHandler $paragraphsFileHandler
+   *   Paragraphs file handler.
    * @param \Drupal\pathauto\AliasCleanerInterface $aliasCleaner
    *   Alias cleaner.
    */
-  public function __construct(ModuleHandler $moduleHandler, EntityTypeManagerInterface $entityTypeManager, AliasCleanerInterface $aliasCleaner) {
-    parent::__construct($moduleHandler, $entityTypeManager);
+  public function __construct(ModuleHandler $moduleHandler, EntityTypeManagerInterface $entityTypeManager, ParagraphsFileHandler $paragraphsFileHandler, AliasCleanerInterface $aliasCleaner) {
+    parent::__construct($moduleHandler, $entityTypeManager, $paragraphsFileHandler);
     $this->aliasCleaner = $aliasCleaner;
     $this->entityType = 'node';
+    $this->paragraphsFileHandler = $paragraphsFileHandler;
   }
 
   /**
@@ -72,7 +80,7 @@ class NodeGenerator extends ContentGenerator implements GeneratorInterface {
       $paragraphs = array_filter($paragraphs);
       unset($rawNode['field_content_paragraphs'], $rawNode['field_header_paragraphs'], $rawNode['field_sidebar_right_paragraphs']);
 
-      $this->generateParagraphsForNode($paragraphs, $rawNode);
+      $this->generateParagraphs($paragraphs, $rawNode);
       $this->prepareValues($rawNode);
       $rawNode['path'] = [
         'alias'    => '/degov-demo-content/' . $this->aliasCleaner->cleanString($rawNode['title']),
@@ -132,120 +140,6 @@ class NodeGenerator extends ContentGenerator implements GeneratorInterface {
       $this->setFrontPage($original_front_page);
       \Drupal::configFactory()->getEditable('degov.degov_demo_content')->set('original_front_page', NULL)->save();
     }
-  }
-
-  /**
-   * Generate paragraphs for node.
-   *
-   * @param array $rawParagraphReferences
-   *   Raw paragraph references.
-   * @param array $rawNode
-   *   Raw node.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function generateParagraphsForNode(array $rawParagraphReferences, array &$rawNode): void {
-    foreach ($rawParagraphReferences as $type => $rawParagraphReferenceElements) {
-      foreach ($rawParagraphReferenceElements as $rawParagraphReference) {
-        $rawParagraph = $this->loadDefinitionByNameTag('paragraphs', $rawParagraphReference);
-        $this->prepareValues($rawParagraph);
-        $this->resolveEncapsulatedParagraphs($rawParagraph);
-        $paragraph = Paragraph::create($rawParagraph);
-        $paragraph->save();
-        $rawNode[$type][] = $paragraph;
-      }
-    }
-  }
-
-  /**
-   * Resolve encapsulated paragraphs.
-   *
-   * @param array $rawParagraph
-   *   Raw paragraph.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function resolveEncapsulatedParagraphs(array &$rawParagraph): void {
-    foreach ($rawParagraph as $index => $rawField) {
-      if (\is_array($rawField)) {
-        foreach ($rawField as $innerIndex => $rawValue) {
-          $fieldName = preg_replace('/(paragraph|media)_reference_/', '', $rawValue);
-          if (strpos($rawValue, 'paragraph_reference_') !== FALSE) {
-            $rawInnerParagraph = $this->loadDefinitionByNameTag('paragraphs', $fieldName);
-            $this->prepareValues($rawInnerParagraph);
-            $innerParagraph = Paragraph::create($rawInnerParagraph);
-            $innerParagraph->save();
-            $rawParagraph[$index][$innerIndex] = $innerParagraph;
-          }
-          if (strpos($rawValue, 'media_reference_') !== FALSE) {
-            $rawMedia = $this->loadDefinitionByNameTag('media', $fieldName);
-            $mediaIds = \Drupal::entityTypeManager()
-              ->getStorage('media')
-              ->getQuery()
-              ->condition('name', $rawMedia['name'])
-              ->execute();
-            if (count($mediaIds) > 0) {
-              $media = Media::load(reset($mediaIds));
-              if ($media instanceof MediaInterface) {
-                $rawParagraph[$index][$innerIndex] = $media->id();
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * Generate node reference paragraphs.
-   *
-   * @param \Drupal\node\Entity\Node $teaserPage
-   *   Teaser page node.
-   * @param array $nodeIds
-   *   Node IDs.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function generateNodeReferenceParagraphs(Node $teaserPage, array $nodeIds): void {
-    $paragraphs = [];
-    foreach ($this->loadDefinitionByNameType('paragraphs', 'node_reference') as $rawParagraph) {
-      $rawParagraph['field_sub_title'] = empty($rawParagraph['field_sub_title']) ? $this->generateBlindText(3) : $rawParagraph['field_sub_title'];
-      $rawParagraph['field_node_reference_nodes'] = $nodeIds;
-      $paragraph = Paragraph::create($rawParagraph);
-      $paragraph->save();
-      $paragraphs[] = $paragraph;
-    }
-    $teaserPage->set('field_content_paragraphs', $paragraphs);
-    $teaserPage->save();
-  }
-
-  /**
-   * Generates Media reference paragraph.
-   *
-   * @param \Drupal\node\Entity\Node $teaserPage
-   *   The Node that should contain the teaser paragraphs.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function generateMediaReferenceParagraphs(Node $teaserPage): void {
-    $rawParagraph = $this->loadDefinitionByNameTag('paragraphs', 'media_reference_citation_front');
-    $this->prepareValues($rawParagraph);
-    $this->resolveEncapsulatedParagraphs($rawParagraph);
-    unset($rawParagraph['field_title']);
-
-    $paragraph = Paragraph::create($rawParagraph);
-    $paragraph->save();
-
-    $teaserPage->get('field_content_paragraphs')->appendItem($paragraph);
-    $teaserPage->save();
   }
 
   /**
