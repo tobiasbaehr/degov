d8c531e0f93a67ac83fa8ff3f1f2c8415d3bdfd2
tobiasb <tobias.baehr@gzevd.de>
2020-09-02 16:38:29 +0200

    Only remove the attribute.

--- modules/media_file_links/src/Plugin/Filter/LinkitFilterExtended.php
+++ modules/media_file_links/src/Plugin/Filter/LinkitFilterExtended.php
@@ -7,9 +7,6 @@ namespace Drupal\media_file_links\Plugin\Filter;
 use Drupal\Component\Utility\Html;
 use Drupal\filter\FilterProcessResult;
 use Drupal\linkit\Plugin\Filter\LinkitFilter;
-use Drupal\media_file_links\Service\MediaFileLinkPlaceholderHandler;
-use Drupal\media_file_links\Service\MediaFileLinkResolver;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a Linkit filter.
@@ -27,67 +24,19 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 final class LinkitFilterExtended extends LinkitFilter {
 
   /**
-   * @var \Drupal\media_file_links\Service\MediaFileLinkResolver
-   */
-  protected $fileLinkResolver;
-
-  /**
-   * @var \Drupal\media_file_links\Service\MediaFileLinkPlaceholderHandler
-   */
-  protected $placeholderHandler;
-
-  /**
-   * @param \Drupal\media_file_links\Service\MediaFileLinkResolver $file_link_resolver
-   */
-  public function setFileLinkResolver(MediaFileLinkResolver $file_link_resolver): void {
-    $this->fileLinkResolver = $file_link_resolver;
-  }
-
-  /**
-   * @param \Drupal\media_file_links\Service\MediaFileLinkPlaceholderHandler $placeholder_handler
-   */
-  public function setPlaceholderHandler(MediaFileLinkPlaceholderHandler $placeholder_handler): void {
-    $this->placeholderHandler = $placeholder_handler;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->setFileLinkResolver($container->get('media_file_links.file_link_resolver'));
-    $instance->setPlaceholderHandler($container->get('media_file_links.placeholder_handler'));
-    return $instance;
-  }
-
-  /**
    * {@inheritDoc}
    */
   public function process($text, $langcode): FilterProcessResult {
-    $has_media_media_links = FALSE;
-    while (($mediaId = $this->placeholderHandler->getMediaIdFromPlaceholder($text)) !== NULL) {
-      $fileUrl = $this->fileLinkResolver->getFileUrlString($mediaId);
-      $text = str_replace($this->placeholderHandler->getPlaceholderForMediaId($mediaId), $fileUrl, $text);
-      $has_media_media_links = TRUE;
-    }
-
-    if ($has_media_media_links && strpos($text, 'data-entity-type') !== FALSE) {
+    if (strpos($text, 'data-entity-type') !== FALSE) {
       $dom = Html::load($text);
       $xpath = new \DOMXPath($dom);
 
       foreach ($xpath->query('//a[@data-entity-type]') as $element) {
         /** @var \DOMElement $element */
-        try {
-          if ($element->getAttribute('data-entity-type') === 'media') {
-            $element->removeAttribute('data-entity-type');
-          }
-
-        }
-        catch (\Exception $e) {
-          watchdog_exception('media_file_links', $e);
+        if ($element->getAttribute('data-entity-type') === 'media' && preg_match('/[\[<]media\/file\/([\d]+)[\]>]/', $element->getAttribute('href'))) {
+          $element->removeAttribute('data-entity-type');
         }
       }
-
       $text = Html::serialize($dom);
     }
 
