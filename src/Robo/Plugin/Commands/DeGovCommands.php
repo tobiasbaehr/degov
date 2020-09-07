d6d328df6b9b2b3943745a216b90197068f465ef
Marc-Oliver Teschke <teschke@publicplan.de>
2020-09-07 12:45:36 +0000

    Merged in feature/degov-1304-core-version-requirement (pull request #729)
    Added core_version_requirement for Drupal 9
    
    * Added core_version_requirement for Drupal 9
    
    * Merged release/8.5.x-dev into feature/degov-1304-core-version-requirement

--- src/Robo/Plugin/Commands/DeGovCommands.php
+++ src/Robo/Plugin/Commands/DeGovCommands.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\degov\Robo\Plugin\Commands;
 
+use Drupal\Core\Serialization\Yaml;
 use Robo\Tasks;
 use degov\Scripts\Robo\Model\InstallationProfile;
 use degov\Scripts\Robo\Model\InstallationProfileCollection;
 use degov\Scripts\Robo\RunsTrait;
+use Symfony\Component\Finder\Finder;
 
 /**
  * This is project's console commands configuration for Robo task runner.
@@ -49,6 +51,40 @@ class DeGovCommands extends Tasks {
 
     $installationProfileCollection->setMainInstallationProfile($mainInstallationProfile);
     $this->runDrushProfileInstallation($installationProfileCollection);
+  }
+
+  /**
+   * Normalize info files.
+   *
+   * @param string $path Path to a directory with info files.
+   * @param string $package Package name in case non is set.
+   */
+  public function degovNormalizeInfoFiles(string $path, string $package = 'deGov') {
+    $finder = new Finder();
+    $files = $finder->files()->in(trim($path, DIRECTORY_SEPARATOR))->name('*.info.yml');
+    foreach ($files as $file) {
+      $yaml = Yaml::decode(file_get_contents($file->getPathname()));
+      $new_yaml = ['name' => $yaml['name']];
+      unset($yaml['name']);
+      $new_yaml += ['type' => $yaml['type']];
+      unset($yaml['type']);
+
+      if (!empty($yaml['description'])) {
+        $new_yaml += ['description' => trim(str_replace('\"', '"', $yaml['description']))];
+        unset($yaml['description']);
+      }
+
+      if (empty($yaml['package'])) {
+        $yaml['package'] = $package;
+      }
+      $new_yaml += ['package' => $yaml['package']];
+      unset($yaml['package']);
+      $new_yaml += ['core_version_requirement' => '^8.8 || ^9'];
+      unset($yaml['core_version_requirement']);
+      unset($yaml['core']);
+      $new_yaml += $yaml;
+      file_put_contents($file->getPathname(), Yaml::encode($new_yaml));
+    }
   }
 
 }
