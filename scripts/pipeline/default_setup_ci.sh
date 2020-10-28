c6e72c77e81d9b17441a291a3994db0830d5e9ff
tobiasb <tobias.baehr@gzevd.de>
2020-10-28 23:47:36 +0100

    WIP

--- scripts/pipeline/default_setup_ci.sh
+++ scripts/pipeline/default_setup_ci.sh
@@ -5,23 +5,28 @@ set -o pipefail
 set -o errexit
 
 # shellcheck disable=SC2164
-__DIR__="$(cd "$(dirname "${0}")"; pwd)"
+__DIR__="$(
+  cd "$(dirname "${0}")"
+  pwd
+)"
 
 # shellcheck source=.
 source "$__DIR__/.env"
-if [[ -n "${DEBUG:-}" ]];then
+if [[ -n ${DEBUG:-} ]]; then
   set -o xtrace
 fi
 
 main() {
-  ln -sf "$BITBUCKET_CLONE_DIR/php_error.log" /tmp/php_error.log
-  echo "$BITBUCKET_DOCKER_HOST_INTERNAL host.docker.internal" >> /etc/hosts
-  if [[ ! -d "$PROFILE_DIR" ]];then
+  ln -sf "$CI_ROOT_DIR/php_error.log" /tmp/php_error.log
+  if [[ -n ${BITBUCKET_DOCKER_HOST_INTERNAL:-} ]]; then
+    echo "$BITBUCKET_DOCKER_HOST_INTERNAL host.docker.internal" >> /etc/hosts
+  fi
+  if [[ ! -d $CI_ROOT_DIR ]]; then
     # Restore the data which was deleted in composer_setup.sh and is not part of the artifact.
-    mkdir "$BITBUCKET_CLONE_DIR/project/docroot/profiles/contrib"
-    mkdir "$BITBUCKET_CLONE_DIR/testing/lfs_data"
-    mv -v "$BITBUCKET_CLONE_DIR/project/$CONTRIBNAME-stable-$DB_DUMP_VERSION.sql.gz" "$BITBUCKET_CLONE_DIR/testing/lfs_data/$CONTRIBNAME-stable-$DB_DUMP_VERSION.sql.gz"
-    (cd "$BITBUCKET_CLONE_DIR" && rsync -az --exclude="project/" . "$PROFILE_DIR")
+    mkdir "$CI_ROOT_DIR/project/docroot/profiles/contrib"
+    mkdir "$CI_ROOT_DIR/testing/lfs_data"
+    mv -v "$CI_ROOT_DIR/project/$CONTRIBNAME-stable-$DB_DUMP_VERSION.sql.gz" "$CI_ROOT_DIR/testing/lfs_data/$CONTRIBNAME-stable-$DB_DUMP_VERSION.sql.gz"
+    (cd "$CI_ROOT_DIR" && rsync -az --exclude="project/" . "$PROFILE_DIR")
   fi
 }
 
