995ab9457fafc234d668120471b14e3996ebf849
tobiasb <tobias.baehr@gzevd.de>
2020-10-29 18:23:52 +0100

    WIP

--- scripts/ci/tests--integration.sh
+++ scripts/ci/tests--integration.sh
@@ -25,10 +25,8 @@ source "$__DIR__/common_functions.sh"
 
 main() {
   cd "$CI_ROOT_DIR/project"
-  ls -lah
   _info "### Importing db dump"
-  _drush sql-query --file="$CONTRIBNAME.sql"
-
+  _drush sql-query --file="$CI_ROOT_DIR/project/$CONTRIBNAME.sql"
   _info "### Running Behat features by tags: $1"
   set +e
   _disable_geocoder_presave 0
