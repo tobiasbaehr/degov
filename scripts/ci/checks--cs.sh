4b074185a3b5b995b2f47112dec3c4f365204e47
tobiasb <tobias.baehr@gzevd.de>
2020-10-29 14:11:50 +0100

    WIP

---
+++ scripts/ci/checks--cs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+if [[ -n ${DEBUG:-} ]]; then
+  set -o xtrace
+fi
+
+# shellcheck disable=SC2164
+__DIR__="$(
+  cd "$(dirname "${0}")"
+  pwd
+)"
+
+# shellcheck source=.
+source "$__DIR__/.env"
+if [[ -n ${DEBUG:-} ]]; then
+  set -o xtrace
+fi
+
+_info() {
+  local color_info='\x1b[96m'
+  local color_reset='\x1b[0m'
+  echo -e "$(printf '%s%s%s\n' "$color_info" "$@" "$color_reset")"
+}
+
+main() {
+  _info "### Check php compatibility"
+  phpcs -p -s --standard=phpcompatibility.xml
+  _info "### Check coding standards"
+  phpcs --report=junit --report-file="$CI_ROOT_DIR/test-reports/phpcs/phpcs-junit.xml"
+}
+
+main
---
+++ scripts/ci/tests--phpunit.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+if [[ -n ${DEBUG:-} ]]; then
+  set -o xtrace
+fi
+
+# shellcheck disable=SC2164
+__DIR__="$(
+  cd "$(dirname "${0}")"
+  pwd
+)"
+
+# shellcheck source=.
+source "$__DIR__/.env"
+if [[ -n ${DEBUG:-} ]]; then
+  set -o xtrace
+fi
+
+_info() {
+  local color_info='\x1b[96m'
+  local color_reset='\x1b[0m'
+  echo -e "$(printf '%s%s%s\n' "$color_info" "$@" "$color_reset")"
+}
+
+main() {
+  _info "### Running PHPUnit and KernelBase tests"
+  (cd "docroot/profiles/contrib/$CONTRIBNAME" && phpunit --colors=auto --log-junit "$CI_ROOT_DIR/test-reports/phpunit/phpunit-junit.xml" --testdox)
+}
+
+main
