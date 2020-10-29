b4b6e14b26ce52f601ecaf8fd6f508f338f1689e
tobiasb <tobias.baehr@gzevd.de>
2020-10-29 16:41:48 +0100

    WIP

--- scripts/ci/settings.local.php
+++ scripts/ci/settings.local.php
@@ -17,6 +17,7 @@ $settings["file_private_path"] = "sites/default/files/private";
 $settings["file_public_path"] = "sites/default/files";
 $settings["file_temp_path"] = "/tmp";
 $settings["trusted_host_patterns"] = ["^127.0.0.1$","^localhost$","^host.docker.internal$"];
+$settings["hash_salt"] = "7asdiugasd8f623gjwgasgf7a8stfasjdfsdafasdfasdfasdf";
 $config["locale.settings"]["translation"]["path"] = "sites/default/files/translations";
 $config["swiftmailer.transport"]["transport"] = "spool";
 $config["swiftmailer.transport"]["spool_directory"] = "/tmp/swiftmailer-spool";
