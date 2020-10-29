764c99e78bc0cdf66317afe3b8668c778765e810
tobiasb <tobias.baehr@gzevd.de>
2020-10-29 17:17:11 +0100

    WIP

--- scripts/ci/tests--integration.sh
+++ scripts/ci/tests--integration.sh
@@ -29,22 +29,15 @@ main() {
   _setup_file_system
   _setup_local_settings_php
   _info "### Importing db dump"
-  _drush sql-query --file="$TEST_DIR/lfs_data/$CONTRIBNAME-stable-$DB_DUMP_VERSION.sql.gz"
-  _info "### Clear cache"
-  _drush cr
-  _copy_assets
-  _info "### Delete old watchdog entries from db dump"
-  _drush watchdog:delete all
-  _info "### Run database updates"
-  _drush updb
-  _info "### Clear cache"
-  _drush cr
-  _disable_geocoder_presave
-  _info "### Install the degov_demo_content"
-  _drush en degov_demo_content
-  _update_translations
+  _drush sql-query --file="$CONTRIBNAME.sql.gz"
+
+  _info "### Running Behat features by tags: $1"
+  set +e
+  _disable_geocoder_presave 0
+  _behat -c behat.dist.yml --suite=default --tags="$1"
+  EXIT_CODE=$?
   _drush_watchdog
-  _create_db_dump
+  exit $EXIT_CODE
 }
 
 main
